8147b058588517476543be017320d57cd7ffbc44
Alec Robitaille <robit.alec@gmail.com>
2017-08-05 16:34:22 -0230

    fst time groups (split from spatial grouping)

---
+++ R/time-groups.R
@@ -0,0 +1,25 @@
+#' Time Groups
+#'
+#' Assign a numerical group ID for rows/locs sorted by provided time column. If
+#' using GPS collar data, it is recommended to round this time column providing
+#' the rounding unit as described below.
+#'
+#' The rounding unit must be provided
+#'
+#' Note that this numerical time group ID is added to the provided data.table.
+#'
+#' @param dt input locs/rows
+#' @param time time column name
+#' @param roundunit unit by which the time should be rounded (optional)
+#'
+#' @return
+#' @export
+#'
+#' @examples
+time_groups <- function(dt, time, roundunit = NULL) {
+  if(is.null(roundunit)) {
+    dt[, timeGroup := .GRP, by = time]
+  } else {
+    dt[, timeGroup := .GRP, by = round(time)]
+  }
+}
