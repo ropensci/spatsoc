70ad44ea22e59273570b4efdf3ee1637a7a045d3
Alec Robitaille <robit.alec@gmail.com>
2017-09-06 20:42:14 -0230

    fix GroupPts dt -> DT

--- R/group-pts.R
+++ R/group-pts.R
@@ -27,14 +27,14 @@
 #' groups <- GroupPts(spPts = locsPts)
 GroupPts <- function(DT, bufferWidth, timeField = NULL, projection, coordFields = c('EASTING', 'NORTHING'),
                      idField = 'ID', spPts = NULL){
-  if(any(!(c(timeField, idField, coordFields) %in% colnames(dt)))){
-    stop('some fields provided are not present in data.table provided/colnames(dt)')
+  if(any(!(c(timeField, idField, coordFields) %in% colnames(DT)))){
+    stop('some fields provided are not present in data.table provided/colnames(DT)')
   }
 
   if(is.null(timeField)){
     if(is.null(spPts)){
-      if(is.null(dt)) stop("must provide either pts or dt")
-      spPts <- BuildPts(dt, projection, coordFields, idField)
+      if(is.null(DT)) stop("must provide either pts or DT")
+      spPts <- BuildPts(DT, projection, coordFields, idField)
     }
     buffers <- rgeos::gBuffer(spPts, width = bufferWidth, byid = FALSE)
 
@@ -47,15 +47,16 @@ GroupPts <- function(DT, bufferWidth, timeField = NULL, projection, coordFields 
   } else {
     if(!is.null(spPts)) stop("if providing a spPts, cannot provide a time field")
 
-    dt[, {spPts <- BuildPts(.SD, projection, coordFields, idField)
+    DT[, {spPts <- BuildPts(.SD, projection, coordFields, idField)
 
           buffers <- rgeos::gBuffer(spPts, width = bufferWidth, byid = FALSE)
 
           ovr <- sp::over(spPts, sp::disaggregate(buffers))
 
-          setNames(c(as.list(as.data.frame(spPts@coords)),
-                     list(spPts$id, paste(ovr, .GRP, sep = '_'))),
-                   c(coordFields, idField, 'group'))
+          setNames(
+            c(as.list(as.data.frame(spPts@coords)),
+              list(spPts$id, paste(.GRP, ovr, sep = '_'))),
+            c(coordFields, idField, 'group'))
           },
        by = timeField, .SDcols = c(coordFields, idField)]
   }
