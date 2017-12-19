780ea3837c2a96f981154cc9fc68c7d1a4f7b3da
Alec Robitaille <robit.alec@gmail.com>
2017-12-18 22:05:56 -0330

    hourly sol'n

--- R/randomizations.R
+++ R/randomizations.R
@@ -7,7 +7,7 @@
 #'   trajectory randomizations (Spiegel et al. 2016).
 #' @param idField field indicating the id in the input data.table
 #' @param groupField field indicating the group membership for each id
-#' @param dateField (optional) date field used when randomType == 'daily' or 'spiegel'
+#' @param timeField (optional) time field used for providing datetime or hour field or group time field
 #' @inheritParams BuildPts
 #'
 #' @seealso
@@ -18,14 +18,16 @@ Randomizations <- function(DT, idField, groupField, randomType, dateField = NULL
     stop('some fields provided are not present in data.table provided/colnames(DT)')
   }
   if(randomType == 'hourly'){
-    # TODO: this isn't really 'hourly' it's just not 'daily'
-    if(!is.null(dateField)) warning('dateField ignored since randomType is hourly')
+    # if(!is.null(dateField)) warning('dateField ignored since randomType is hourly')
+    #
+    # lsIDs <- unique(DT[[idField]])
+    # randIDs <- sample(lsIDs)
+    #
+    # DT[, randomID := randIDs[.GRP], by = idField]
 
-    lsIDs <- unique(DT[[idField]])
-    randIDs <- sample(lsIDs)
+    DT[, randomID := sample(get(idField)), by = get(dateField)]
 
-    DT[, randomID := randIDs[.GRP], by = idField]
-
+    return(DT[])
   } else if(randomType == 'daily'){
     if(is.null(dateField)) stop('must provide a dateField if daily randomType chosen')
     listIDs <- unique(DT[[idField]])
