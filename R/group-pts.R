0b8806eb3c8f2484107d9ed896abc3c132996463
Alec Robitaille <robit.alec@gmail.com>
2018-02-12 12:08:42 -0330

    rm old GroupPts functions, tidy commented lines

--- R/group-pts.R
+++ R/group-pts.R
@@ -27,81 +27,6 @@
 #'
 #' groups <- GroupPts(spPts = locsPts)
 GroupPts <- function(DT, bufferWidth, timeField = NULL, groupFields = NULL,
-                     projection, coordFields = c('EASTING', 'NORTHING'),
-                     idField = 'ID', spPts = NULL){
-
-  if(!is.null(DT) && any(!(c(timeField, idField, coordFields) %in% colnames(DT)))){
-    stop('some fields provided are not present in data.table provided/colnames(DT)')
-  }
-  if(!is.null(DT) & "group" %in% colnames(DT)) warning("`group` column will be overwritten by this function")
-  if(is.null(timeField)){
-    if(is.null(spPts)){
-      if(is.null(DT)){
-        stop("must provide either pts or DT")
-      }
-      spPts <- BuildPts(DT, projection, coordFields, idField)
-    }
-    buffers <- rgeos::gBuffer(spPts, width = bufferWidth, byid = FALSE)
-    DT[, group := sp::over(spPts, sp::disaggregate(buffers))][]
-  } else {
-    if(is.null(groupFields)) byFields <- timeField else byFields <- c(groupFields, timeField)
-    if(!is.null(spPts)) stop("if providing a spPts, cannot provide a time field")
-
-    ovrDT <- DT[, {spPts <- BuildPts(.SD, projection, coordFields, idField)
-    buffers <- rgeos::gBuffer(spPts, width = bufferWidth, byid = FALSE)
-    ovr <- sp::over(spPts, sp::disaggregate(buffers))
-    data.table::setnames(
-      data.table::data.table(get(idField),
-                             unlist(ovr)),
-      c(idField, 'withinGroup'))
-    },
-    by = byFields, .SDcols = c(coordFields, idField)]
-
-    DT[ovrDT, withinGroup := withinGroup, on = c(idField, byFields)]
-    DT[, group := .GRP, by = c(byFields, 'withinGroup')][, withinGroup := NULL][]
-  }
-}
-
-#' @export
-GroupPtsSF <- function(DT, bufferWidth, timeField = NULL, groupFields = NULL,
-                       projection, coordFields = c('EASTING', 'NORTHING'),
-                       idField = 'ID', spPts = NULL){
-
-  if(!is.null(DT) && any(!(c(timeField, idField, coordFields) %in% colnames(DT)))){
-    stop('some fields provided are not present in data.table provided/colnames(DT)')
-  }
-  if(!is.null(DT) & "group" %in% colnames(DT)) warning("`group` column will be overwritten by this function")
-  if(is.null(timeField)){
-    if(is.null(spPts)){
-      if(is.null(DT)){
-        stop("must provide either pts or DT")
-      }
-      pts <- sf::st_as_sf(DT, coords = c('EASTING', 'NORTHING'))
-      sf::st_crs(pts) <- projection
-    }
-    bufs <- sf::st_buffer(pts, bufferWidth)
-    un <- sf::st_cast(sf::st_union(bufs), 'POLYGON')
-    DT[, group := unlist(sf::st_intersects(pts, un), FALSE, FALSE)][]
-  } else {
-    if(is.null(groupFields)) byFields <- timeField else byFields <- c(groupFields, timeField)
-    if(!is.null(spPts)) stop("if providing a spPts, cannot provide a time field")
-
-    DT[, withinGroup := {
-      pts <- sf::st_as_sf(.SD, coords = coordFields)
-      sf::st_crs(pts) <- projection
-      bufs <- sf::st_buffer(pts, 50)
-      un <- sf::st_cast(sf::st_union(bufs), 'POLYGON')
-      unlist(sf::st_intersects(pts, un), FALSE, FALSE)
-    },
-    by = byFields, .SDcols = c(coordFields, idField)]
-
-    # DT[ovrDT, withinGroup := withinGroup, on = c(idField, byFields)]
-    DT[, groupSF := .GRP, by = c(byFields, 'withinGroup')][, withinGroup := NULL][]
-  }
-}
-
-#' @export
-GroupPtsIGRAPH <- function(DT, bufferWidth, timeField = NULL, groupFields = NULL,
                            projection, coordFields = c('EASTING', 'NORTHING'),
                            idField = 'ID', spPts = NULL){
 
@@ -123,14 +48,8 @@ GroupPtsIGRAPH <- function(DT, bufferWidth, timeField = NULL, groupFields = NULL
       distMatrix <- as.matrix(dist(.SD[, coordFields, with = FALSE]))
       graphAdj <- igraph::graph_from_adjacency_matrix(distMatrix <= bufferWidth)
       igraph::clusters(graphAdj)$membership
-      # clsts
-      # data.table::setnames(
-      # data.table::data.table(get(idField), clstrs)
-      # c(idField, 'withinGroup'))
     },
     by = byFields, .SDcols = c(coordFields, idField)]
-    # ovrDT
-    # DT[ovrDT, withinGroup := withinGroup, on = c(idField, byFields)]
     DT[, groupIG := .GRP, by = c(byFields, 'withinGroup')][, withinGroup := NULL][]
   }
 }
