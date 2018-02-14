988f2663da1dd6e8747e44f9bdb51606af76d09b
Alec Robitaille <robit.alec@gmail.com>
2018-02-14 11:40:38 -0330

    build points no longer needed (?)

--- R/build-pts.R
+++ R/build-pts.R
@@ -1,21 +1,20 @@
 #' Build Points
 #'
-#' Build a SpatialPointsDataFrame from provided data for input to GroupPts or
-#' other uses.
+#' Build a SpatialPointsDataFrame from provided data.table.
 #'
 #' The parameter projection can be built by finding the EPSG code for your
 #' given projection at <http://spatialreference.org/ref/epsg/> and providing it with
 #' "+init=epsg:CODE" (eg: "+init=epsg:4326").
 #'
-#' @param DT Input data with coordinate and id fields. If not provided, fields
-#'   default to c('EASTING', 'NORTHING') and 'ID'
-#' @param projection Character string for input to sp::CRS() and proj4string()
+#' @param DT Input data.table with coordinate and id fields. If not provided, fields
+#'   default to c('EASTING', 'NORTHING') and 'ID'.
+#' @param projection Character string for input to sp::CRS() and proj4string().
 #' @param coordFields Character vector indicating the X coordinate and Y
 #'   coordinate columns. eg: c('EASTING', 'NORTHING')
 #' @param idField Character string indicating the column name for the ID
-#'   associated with each point
+#'   associated with each point.
 #'
-#' @return SpatialPointsDataFrame for each ID provided
+#' @return SpatialPointsDataFrame
 #' @export
 #'
 #' @import data.table
@@ -28,5 +27,3 @@ BuildPts <- function(DT, projection, coordFields = c('EASTING', 'NORTHING'), idF
                              data = DT[, .(id = get(idField))])
 }
 
-# TODO: check if DT is a data.table
-# TODO: stopif null projection
