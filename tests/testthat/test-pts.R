eae94aeea22d4ed4bb2067ea16ae2d20930736d4
Alec Robitaille <robit.alec@gmail.com>
2018-05-29 17:07:38 -0700

    must provide column names tests

--- tests/testthat/test-pts.R
+++ tests/testthat/test-pts.R
@@ -2,9 +2,36 @@
 context('test GroupPts')
 library(spatsoc)
 
-DT <- fread('..testdata/Buffalo')
+DT <- fread('../testdata/buffalo.csv')
+# DT <- fread('input/Buffalo.csv')
 
 ls.params <- list(DT = DT,
                   coordFields = c('X', 'Y'),
                   idField = 'ID',
                   time = 'posix')
+
+
+test_that("column names must be provided", {
+  expect_error(GroupPts(DT, coordFields = NULL))
+  expect_error(GroupPts(DT, idField = NULL))
+  expect_error(GroupPts(DT, time = NULL))
+  expect_error(GroupPts(DT, coordFields = NULL, time = NULL))
+  expect_error(GroupPts(DT, idField = NULL, time = NULL))
+  expect_error(GroupPts(DT, idField = NULL, coordFields = NULL))
+  expect_error(GroupPts(DT, idField = NULL, coordFields = NULL, time = NULL))
+})
+
+test_that("column names must exist in DT", {
+  expect_error(GroupPts(
+    DT,
+    idField = 'ID',
+    time = 'potato',
+    coordFields = c('X', 'Y')
+  ))
+  # expect_error(GroupPts(DT, idField = 'potato'))
+  # expect_error(GroupPts(DT, time = 'potato'))
+  # expect_error(GroupPts(DT, coordFields = NULL, time = NULL))
+  # expect_error(GroupPts(DT, idField = NULL, time = NULL))
+  # expect_error(GroupPts(DT, idField = NULL, coordFields = NULL))
+  # expect_error(GroupPts(DT, idField = NULL, coordFields = NULL, time = NULL))
+})
