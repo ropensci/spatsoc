4d08d7b6085f63b01609743562a183e064b9f780
Alec Robitaille <robit.alec@gmail.com>
2018-05-30 15:18:04 -0700

    drop redundant tests for columns being provided
    only distance threshold and ID are mandatory and have their own specifics if-checks

--- tests/testthat/test-pts.R
+++ tests/testthat/test-pts.R
@@ -11,27 +11,33 @@ ls.params <- list(DT = DT,
                   time = 'posix')
 
 
-test_that("column names must be provided", {
-  expect_error(GroupPts(DT, coordFields = NULL))
-  expect_error(GroupPts(DT, idField = NULL))
-  expect_error(GroupPts(DT, time = NULL))
-  expect_error(GroupPts(DT, coordFields = NULL, time = NULL))
-  expect_error(GroupPts(DT, idField = NULL, time = NULL))
-  expect_error(GroupPts(DT, idField = NULL, coordFields = NULL))
-  expect_error(GroupPts(DT, idField = NULL, coordFields = NULL, time = NULL))
+test_that('column names must be provided', {
+  expect_error(GroupPts(DT, distance = 10),
+               'ID field required', fixed = FALSE)
+
+  expect_error(GroupPts(DT, distance = 10),
+               'ID field required', fixed = FALSE)
+  expect_error(GroupPts(DT, distance = 10, coordFields = NULL, time = NULL),
+               'some fields', fixed = FALSE)
+  expect_error(GroupPts(DT, distance = 10, idField = NULL, time = NULL),
+               'some fields', fixed = FALSE)
+  expect_error(GroupPts(DT, distance = 10, idField = NULL, coordFields = NULL),
+               'some fields', fixed = FALSE)
+  expect_error(GroupPts(DT, distance = 10, idField = NULL, coordFields = NULL,
+                        time = NULL), 'some fields', fixed = FALSE)
 })
 
-test_that("column names must exist in DT", {
-  expect_error(GroupPts(
-    DT,
-    idField = 'ID',
-    time = 'potato',
-    coordFields = c('X', 'Y')
-  ))
-  # expect_error(GroupPts(DT, idField = 'potato'))
-  # expect_error(GroupPts(DT, time = 'potato'))
-  # expect_error(GroupPts(DT, coordFields = NULL, time = NULL))
-  # expect_error(GroupPts(DT, idField = NULL, time = NULL))
-  # expect_error(GroupPts(DT, idField = NULL, coordFields = NULL))
-  # expect_error(GroupPts(DT, idField = NULL, coordFields = NULL, time = NULL))
-})
+# test_that('column names must exist in DT', {
+#   expect_silent(GroupPts(
+#     DT,
+#     idField = 'ID',
+#     time = 'potato',
+#     coordFields = c('X', 'Y')
+#   ))
+#   # expect_error(GroupPts(DT, idField = 'potato'))
+#   # expect_error(GroupPts(DT, time = 'potato'))
+#   # expect_error(GroupPts(DT, coordFields = NULL, time = NULL))
+#   # expect_error(GroupPts(DT, idField = NULL, time = NULL))
+#   # expect_error(GroupPts(DT, idField = NULL, coordFields = NULL))
+#   # expect_error(GroupPts(DT, idField = NULL, coordFields = NULL, time = NULL))
+# })
