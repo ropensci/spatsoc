633c00d21c985d7c312ab0ca91dda522e73ae5ed
Alec Robitaille <robit.alec@gmail.com>
2018-05-31 11:24:11 -0700

    time group fields already found in DT

--- tests/testthat/test-time.R
+++ tests/testthat/test-time.R
@@ -24,3 +24,17 @@ test_that('if threshold is null, warning returned', {
   expect_warning(GroupTimes(copyDT, timeField = 'posix', threshold = NULL),
                  'no threshold provided', fixed = FALSE)
 })
+
+
+test_that('time fields are already present', {
+  copyDT <- copy(DT)[, posix := as.POSIXct(posix)]
+  GroupTimes(copyDT, timeField = 'posix', threshold = '10 minutes')
+  expect_warning(GroupTimes(copyDT, timeField = 'posix', threshold = '10 minutes'),
+                 'columns found in input DT', fixed = FALSE)
+})
+
+
+# warn if greater than 60 minutes
+# warn if not divisible
+# warn if block isnt even
+# stop if threshold isnt in terms of hours, minutes, days
