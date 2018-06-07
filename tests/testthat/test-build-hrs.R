d1305ef96ec07a52078637f0805f1864b8e6b2c7
Alec Robitaille <robit.alec@gmail.com>
2018-06-07 11:00:48 -0700

    fst test build hrs

---
+++ tests/testthat/test-build-hrs.R
@@ -0,0 +1,47 @@
+# Test Build HRs
+context('test BuildHRs')
+library(spatsoc)
+
+DT <- fread('../testdata/buffalo.csv')
+utm <- '+proj=utm +zone=36 +south +ellps=WGS84 +datum=WGS84 +units=m +no_defs'
+
+
+test_that('DT is required', {
+  expect_error(BuildHRs(DT = NULL, spPts = NULL),
+               'input DT or spPts required')
+})
+
+# test_that('coordFields, idField, projection must be provided and proper format', {
+#   expect_error(BuildHRs(DT = DT, idField = NULL,
+#                           coordFields = c('X', 'Y'),
+#                           projection = utm),
+#                'idField must be provided')
+#
+#   expect_error(BuildHRs(DT = DT, idField = 'ID',
+#                           coordFields = c('X', 'Y'),
+#                           projection = NULL),
+#                'projection must be provided')
+#
+#   expect_error(BuildHRs(DT = DT, idField = 'ID',
+#                           coordFields = NULL,
+#                           projection = utm),
+#                'coordFields must be provided')
+#
+#   expect_error(BuildHRs(DT = DT, idField = 'ID',
+#                           coordFields = c('ID', 'ID'),
+#                           projection = utm),
+#                'coordFields must be numeric')
+# })
+#
+#
+# test_that('column names must exist in DT', {
+#   expect_error(BuildHRs(DT = DT, idField = 'ID',
+#                           coordFields = c('potatoX', 'potatoY'),
+#                           projection = utm),
+#                'not present in input DT', fixed = FALSE)
+#
+#   expect_error(BuildHRs(DT = DT, idField = 'potato',
+#                           coordFields = c('X', 'Y'),
+#                           projection = utm),
+#                'not present in input DT', fixed = FALSE)
+# })
