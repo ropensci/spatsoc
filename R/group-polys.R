b9b03442c58effea923910e2d97d9ca63163bddf
Alec Robitaille <robit.alec@gmail.com>
2018-06-07 17:04:47 -0700

    more big structure changes group polys

--- R/group-polys.R
+++ R/group-polys.R
@@ -35,7 +35,6 @@ GroupPolys <-
            idField = NULL,
            byFields = NULL,
            spPolys = NULL) {
-
     if (is.null(area) | !is.logical(area)) {
       stop('area must be provided (TRUE or FALSE)')
     }
@@ -46,58 +45,123 @@ GroupPolys <-
       stop('cannot provide both DT and spPolys')
 
 
-    if(is.null(byFields)) {
-      if(!is.null(DT) && is.null(spPolys)) {
-        spPolys <-
-          BuildHRs(
-            DT = DT,
-            projection = projection,
-            hrType = hrType,
-            hrParams = hrParams,
-            coordFields = coordFields,
-            idField = idField,
-            byFields = NULL,
-            spPts = NULL
-          )
+      if (is.null(byFields)) {
+        if (!is.null(DT) && is.null(spPolys)) {
+          spPolys <-
+            BuildHRs(
+              DT = DT,
+              projection = projection,
+              hrType = hrType,
+              hrParams = hrParams,
+              coordFields = coordFields,
+              idField = idField,
+              byFields = NULL,
+              spPts = NULL
+            )
+        }
+
+        if (!area) {
+          unionPolys <- rgeos::gUnaryUnion(spPolys)
+          ovr <- sp::over(spPolys, sp::disaggregate(unionPolys))
+          ovrDT <- data.table::data.table(names(ovr),
+                                          ovr)
+          data.table::setnames(ovrDT, c('ID', 'group'))
+          # check if null byfields, if it isnt null split the -
+          return(ovrDT[])
+        } else if (area) {
+          inters <- rgeos::gIntersection(spPolys, spPolys, byid = TRUE)
+          data.table::data.table(area = sapply(
+            inters@polygons,
+            FUN = function(x) {
+              slot(x, 'area')
+            }
+          ) / 1e6)[, # cant actually use 1e6, but how do we standardize units?
+                   # this is susceptible to error if ID field provided has spaces
+                   c('ID1', 'ID2') := data.table::tstrsplit(sapply(
+                     inters@polygons,
+                     FUN = function(x) {
+                       slot(x, 'ID')
+                     }
+                   ),
+                   ' ',
+                   type.convert = TRUE)][]
+        }
+      } else if (!is.null(byFields)) {
+        if (!is.null(spPolys)) {
+          stop('cannot provide spPolys if providing byFields')
+        }
+
+        if (!area) {
+          ovrDT <-
+            DT[, {
+              suppressWarnings(
+                spPolys <-
+                  BuildHRs(
+                    DT = DT,
+                    projection = projection,
+                    hrType = hrType,
+                    hrParams = hrParams,
+                    coordFields = coordFields,
+                    idField = idField,
+                    byFields = NULL,
+                    spPts = NULL
+                  )
+              )
+              if (!is.null(spPolys)) {
+                unionPolys <- rgeos::gUnaryUnion(spPolys)
+                ovr <- sp::over(spPolys, sp::disaggregate(unionPolys))
+                ovrDT <- data.table::data.table(names(ovr), ovr)
+                data.table::setnames(ovrDT, c('ID', 'group'))
+              } else {
+                data.table(ID = get(idField),
+                           withinGroup = -999L)
+              }
+            }, by = byFields, .SDcols = c(coordFields, idField)]
+
+          DT[ovrDT, withinGroup := withinGroup, on = c(idField, byFields)]
+          DT[, group := .GRP, by = c(byFields, 'withinGroup')]
+          DT[withinGroup == -999L, group := NA]
+          set(DT, j = 'withinGroup', value = NULL)
+          return(DT[])
+        } else {
+          #   ovrDT <-
+          #     DT[, {
+          #       suppressWarnings(
+          #         spLines <- BuildLines(
+          #           .SD,
+          #           projection = projection,
+          #           coordFields = coordFields,
+          #           idField = idField
+          #         )
+          #       )
+          #       if (!is.null(spLines)) {
+          #         if (bufferWidth == 0) {
+          #           merged <- rgeos::gBuffer(spLines, width = 0.0001, byid = FALSE)
+          #         } else {
+          #           merged <- rgeos::gBuffer(spLines, width = bufferWidth, byid = FALSE)
+          #         }
+          #         ovr <- sp::over(spLines, sp::disaggregate(merged),
+          #                         returnList = TRUE)
+          #         ovrDT <- data.table::data.table(names(ovr),
+          #                                         unlist(ovr))
+          #         data.table::setnames(ovrDT, c(idField, 'withinGroup'))
+          #       } else {
+          #         data.table(ID = get(idField), withinGroup = -999L)
+          #       }
+          #     }, by = byFields, .SDcols = c(coordFields, idField)]
+          #
+          #   DT[ovrDT, withinGroup := withinGroup, on = c(idField, byFields)]
+          #   DT[, group := .GRP, by = c(byFields, 'withinGroup')]
+          #   DT[withinGroup == -999L, group := NA]
+          #   set(DT, j = 'withinGroup', value = NULL)
+          # }
+          # DT[, {
+          #
+          # }, by = byFields]
+
+          # ovr vor WIHTIN WIHTI
+          print('potato')
+        }
       }
-
-      if (!area) {
-        unionPolys <- rgeos::gUnaryUnion(spPolys)
-        ovr <- sp::over(spPolys, sp::disaggregate(unionPolys))
-        ovrDT <- data.table::data.table(names(ovr),
-                                        ovr)
-        data.table::setnames(ovrDT, c('ID', 'group'))
-        # check if null byfields, if it isnt null split the -
-        return(ovrDT[])
-      } else if (area) {
-        inters <- rgeos::gIntersection(spPolys, spPolys, byid = TRUE)
-        data.table::data.table(area = sapply(
-          inters@polygons,
-          FUN = function(x) {
-            slot(x, 'area')
-          }
-        )/1e6)[, # cant actually use 1e6, but how do we standardize units?
-           # this is susceptible to error if ID field provided has spaces
-           c('ID1', 'ID2') := data.table::tstrsplit(sapply(
-             inters@polygons,
-             FUN = function(x) {
-               slot(x, 'ID')
-             }
-           ),
-           ' ',
-           type.convert = TRUE)][]
-      }
-    } else if (byFields) {
-      if(!is.null(spPolys)) {
-        stop('cannot provide spPolys if providing byFields')
-      }
-
-      DT[, {
-
-      }, by = byFields]
-
-      # ovr vor WIHTIN WIHTI
     }
-}
-
   }
