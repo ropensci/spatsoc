631d8d74dfa2cebcf0222e3aacaf7511752a17c5
Alec Robitaille <robit.alec@gmail.com>
2018-06-14 23:27:13 -0700

    start testing, reviewing randomizations

--- R/randomizations.R
+++ R/randomizations.R
@@ -1,10 +1,17 @@
 #' Randomizations
 #'
+#' Randomization
+#'
+#' Randomization types:
+#'
+#' 'hourly' simply randomly assigns an ID to each location.
+#'
+#' 'daily' will randomize the ID for each individual 24hr trajectory
+#'
+#' 'spiegel' will implement the daily movement trajectory randomizations (Spiegel et al. 2016).
+#'
 #' @param DT input data.table with id, group fields and (optional) time fields
-#' @param randomType one of 'daily', 'hourly' or 'spiegel'. 'daily' will randomize the ID
-#'   for each individual 24hr trajector while 'hourly' simply randomly assigns
-#'   an ID to each location. 'spiegel' will implement the daily movement
-#'   trajectory randomizations (Spiegel et al. 2016).
+#' @param type one of 'daily', 'hourly' or 'spiegel' - see details
 #' @param idField field indicating the id in the input data.table
 #' @param groupField field indicating the group membership for each id
 #' @param timeField (optional) time field used for providing datetime or hour field or group time field
@@ -12,16 +19,23 @@
 #' @param splitBy List of fields in DT to split the randomization process by
 #' @param iterations The number of iterations to randomize
 #'
+
+#'
 #' @seealso
 #'   <http://onlinelibrary.wiley.com/doi/10.1111/2041-210X.12553/full>
 #' @export
-Randomizations <- function(DT, idField, groupField, randomType,
-                           dateField = NULL, splitBy = NULL, iterations = NULL) {
+Randomizations <- function(DT = NULL,
+                           type = NULL,
+                           idField = NULL,
+                           dateField = NULL,
+                           splitBy = NULL,
+                           iterations = NULL
+) {
   if(any(!(c(idField, groupField, dateField) %in% colnames(DT)))){
     stop('some fields provided are not present in data.table provided/colnames(DT)')
   }
 
-  if(!(randomType %in% c('hourly', 'daily', 'spiegel'))) stop('must provide either hourly, daily or spiegel for randomType')
+  if(!(type %in% c('hourly', 'daily', 'spiegel'))) stop('must provide either hourly, daily or spiegel for type')
 
   if(!is.numeric(iterations) & !is.null(iterations)) stop('must provide a numeric for iterations or NULL')
 
@@ -29,13 +43,13 @@ Randomizations <- function(DT, idField, groupField, randomType,
 # warning
 
   if(iterations == 1){
-    if(randomType == 'hourly'){
+    if(type == 'hourly'){
       if(is.null(dateField)) stop('dateField required, please provide datetime field')
       if(is.null(splitBy)) groupFields <- dateField else groupFields <- c(splitBy, dateField)
       DT[, randomID := sample(get(idField)), by = groupFields]
 
       return(DT[])
-    } else if(randomType == 'daily'){
+    } else if(type == 'daily'){
       if(length(intersect(class(DT[[dateField]]), c('POSIXct', 'POSIXt', 'IDate', 'Date'))) == 0){
         stop('provided dateField is not of class POSIXct or IDate, for daily random type
              please provide a datetime column or IDate')
@@ -46,7 +60,7 @@ Randomizations <- function(DT, idField, groupField, randomType,
       dailyIDs[, randomID := sample(ID), by = c(splitBy, 'yday')]
       return(merge(DT, dailyIDs, on = c('yday', splitBy)))
 
-      } else if(randomType == 'spiegel'){
+      } else if(type == 'spiegel'){
         if(length(intersect(class(DT[[dateField]]), c('POSIXct', 'POSIXt', 'IDate', 'Date'))) == 0){
           stop('provided dateField is not of class POSIXct or IDate, for daily random type
                please provide a datetime column or IDate')
@@ -64,12 +78,12 @@ Randomizations <- function(DT, idField, groupField, randomType,
     replicated <- DT[rep(1:.N, iterations + 1)][, iter := seq(0, .N-1, 1), by = rowID]
     replicated[iter == 0, observed := 1]
     replicated[iter != 0, observed := 0]
-    if(randomType == 'hourly'){
+    if(type == 'hourly'){
       if(is.null(dateField)) stop('dateField required, please provide datetime field')
       replicated[observed != 1, randomID := sample(get(idField)), by = c('iter', dateField, splitBy)]
       replicated[observed == 1, randomID := get(idField)]
       return(replicated[])
-      } else if(randomType == 'daily'){
+      } else if(type == 'daily'){
 
         if(length(intersect(class(DT[[dateField]]), c('POSIXct', 'POSIXt', 'IDate', 'Date'))) == 0){
           stop('provided dateField is not of class POSIXct or IDate, for daily random type
@@ -83,7 +97,7 @@ Randomizations <- function(DT, idField, groupField, randomType,
         dailyIDs[observed == 1, randomID := ID]
         return(merge(replicated, dailyIDs, on = c('iter', 'yday', splitBy), all = TRUE))
 
-      } else if(randomType == 'spiegel'){
+      } else if(type == 'spiegel'){
         if(length(intersect(class(DT[[dateField]]), c('POSIXct', 'POSIXt', 'IDate', 'Date'))) == 0){
           stop('provided dateField is not of class POSIXct or IDate, for daily random type
                please provide a datetime column or IDate')
