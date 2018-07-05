7f31b77ba211bb9cd65c7386942fd1260cd23c9c
Alec Robitaille <robit.alec@gmail.com>
2018-07-05 16:29:39 -0700

    adjust if structure to accommodate spPts for build_polys
    tests passing

--- R/build_polys.R
+++ R/build_polys.R
@@ -22,58 +22,62 @@ build_polys <- function(DT = NULL,
     stop('cannot provide both DT and spPts')
   }
 
-  if (is.null(coords)) {
-    stop('coords must be provided')
+  if (!is.null(DT) && is.null(spPts)) {
+    if (is.null(coords)) {
+      stop('coords must be provided')
+    }
+
+    if (is.null(id)) {
+      stop('id must be provided')
+    }
+
+    if (is.null(projection)) {
+      stop('projection must be provided')
+    }
+
+    if (length(coords) != 2) {
+      stop('coords requires a vector of column names for coordinates X and Y')
+    }
+
+    if (any(!(c(id, coords) %in% colnames(DT)))) {
+      stop(paste0(
+        as.character(paste(setdiff(
+          c(id, coords), colnames(DT)
+        ),
+        collapse = ', ')),
+        ' field(s) provided are not present in input DT'
+      ))
+    }
+
+    if (any(!(DT[, vapply(.SD, is.numeric, TRUE),
+                 .SDcols = coords]))) {
+      stop('coords must be numeric')
+    }
+
+    if (is.null(splitBy)) {
+      splitBy <- id
+    } else {
+      splitBy <- c(id, splitBy)
+    }
+
+    if (any(!(DT[, lapply(
+      .SD,
+      FUN = function(x) {
+        is.numeric(x) | is.character(x) | is.integer(x)
+      }
+    ), .SDcols = splitBy]))) {
+      stop('id (and splitBy when provided) must be character, numeric or integer type')
+    }
+
   }
 
-  if (is.null(id)) {
-    stop('id must be provided')
-  }
-
-  if (is.null(projection)) {
-    stop('projection must be provided')
-  }
 
   if (is.null(hrType)) {
     stop('hrType must be provided')
   }
 
-  if (length(coords) != 2) {
-    stop('coords requires a vector of column names for coordinates X and Y')
-  }
-
-  if (any(!(c(id, coords) %in% colnames(DT)))) {
-    stop(paste0(
-      as.character(paste(setdiff(
-        c(id, coords), colnames(DT)
-      ),
-      collapse = ', ')),
-      ' field(s) provided are not present in input DT'
-    ))
-  }
-
-  if (any(!(DT[, vapply(.SD, is.numeric, TRUE), .SDcols = coords]))) {
-    stop('coords must be numeric')
-  }
-
-
   if (is.null(hrParams)) {
     warning('hrParams is not provided, using defaults')
-  }
-
-  if (is.null(splitBy)) {
-    splitBy <- id
-  } else {
-    splitBy <- c(id, splitBy)
-  }
-
-  if (any(!(DT[, lapply(
-    .SD,
-    FUN = function(x) {
-      is.numeric(x) | is.character(x) | is.integer(x)
-    }
-  ), .SDcols = splitBy]))) {
-    stop('id (and splitBy when provided) must be character, numeric or integer type')
   }
 
   if (is.null(spPts)) {
