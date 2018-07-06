6a8ad7fb245b0012eaff13b94a294646191d8897
Alec Robitaille <robit.alec@gmail.com>
2018-07-06 10:26:19 -0700

    rename data column names

--- R/data.R
+++ R/data.R
@@ -1,9 +1,8 @@
 #' Example relocation data
 #'
-#' An example dataset of three individuals
+#' An example dataset of three individuals' relocations
 #'
-#' @format A data.table with 4 columns: ID, x, y, date
-#'
+#' @format A data.table with 4 columns: ID, x, y, datetime
 #'
 #' @source to fill...
 "DT"
