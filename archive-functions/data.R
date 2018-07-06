162d51fbe6de788d95967be8379181703bf3e473
Alec Robitaille <robit.alec@gmail.com>
2018-07-06 13:08:16 -0700

    rm archive-functions

--- archive-functions/data.R
+++
@@ -1,8 +0,0 @@
-#' Example relocation data
-#'
-#' An example dataset of three individuals' relocations
-#'
-#' @format A data.table with 4 columns: ID, x, y, datetime
-#'
-#' @source to fill...
-"DT"
