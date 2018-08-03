2bb4afcc92570ffedf0336d31b61beee9745945e
Alec Robitaille <robit.alec@gmail.com>
2018-08-03 10:17:22 -0700

    add examples build_polys

--- R/build_polys.R
+++ R/build_polys.R
@@ -27,12 +27,45 @@
 #'
 #' @importFrom sp SpatialPointsDataFrame CRS
 #' @importFrom adehabitatHR mcp kernelUD getverticeshr
+#'
+#'
+#' @examples
+#' # Load data.table
+#' library(data.table)
+#'
+#' # Read example data
+#' DT <- fread(system.file("extdata", "DT.csv", package = "spatsoc"))
+#'
+#' # Cast the character column to POSIXct
+#' DT[, datetime := as.POSIXct(datetime, tz = 'UTC')]
+#'
+#' # Proj4 string for example data
+#' utm <- '+proj=utm +zone=36 +south +ellps=WGS84 +datum=WGS84 +units=m +no_defs'
+#'
+#' # Build polygons for each individual
+#' build_polys(DT, projection = utm, hrType = 'mcp', hrParams = list(percent = 95),
+#'             id = 'ID', coords = c('X', 'Y'))
+#'
+#' # Build polygons for each individual by year
+#' DT[, yr := year(datetime)]
+#' build_polys(DT, projection = utm, hrType = 'mcp', hrParams = list(percent = 95),
+#'             id = 'ID', coords = c('X', 'Y'), splitBy = 'yr')
+#'
+#' # Build polygons from SpatialPointsDataFrame
+#' library(sp)
+#' pts <- SpatialPointsDataFrame(coords = DT[, .(X, Y)],
+#'                               proj4string = CRS(utm),
+#'                               data = DT[, .(ID)]
+#' )
+#'
+#' build_polys(spPts = pts, hrType = 'mcp', hrParams = list(percent = 95))
+#'
 build_polys <- function(DT = NULL,
                         projection = NULL,
                         hrType = NULL,
                         hrParams = NULL,
-                        coords = NULL,
                         id = NULL,
+                        coords = NULL,
                         splitBy = NULL,
                         spPts = NULL) {
   # due to NSE notes in R CMD check
