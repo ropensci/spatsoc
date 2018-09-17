de3f1762c179a3c9829f55be25e22613f468dc02
Alec Robitaille <robit.alec@gmail.com>
2018-09-17 10:42:48 -0230

    fst skeleton

---
+++ R/get_gbi.R
@@ -0,0 +1,25 @@
+#' Generate group by individual matrix
+#'
+#' @inheritParams group_pts
+#' @param
+#'
+#' @export
+#'
+#' @family Spatial grouping
+#' @seealso \code{\link{group_pts}} \code{\link{group_lines}} \code{\link{group_polys}}
+
+
+#' @importFrom
+
+#' @examples
+
+get_gbi <-
+  function(DT = NULL,
+           threshold = NULL,
+           projection = NULL,
+           id = NULL,
+           coords = NULL,
+           timegroup = NULL,
+           sortBy = NULL,
+           splitBy = NULL,
+           spLines = NULL) {
