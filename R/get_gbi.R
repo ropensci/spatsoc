ca87294b641d72264d65cdd67d0cdba2dd91b30b
Alec Robitaille <robit.alec@gmail.com>
2018-09-17 14:54:39 -0230

    add args to gbi man

--- R/get_gbi.R
+++ R/get_gbi.R
@@ -1,17 +1,18 @@
 #' Generate group by individual matrix
 #'
-#' inheritParams group_pts
-#' param
+#' @inheritParams group_pts
+#' @param group Character string of group column (generated from spatsoc's spatial grouping functions)
+#' @param type Character string indicating which spatial grouping function was used. \code{\link{group_pts}}: 'point', \code{\link{group_lines}}: 'line', \code{\link{group_polys}}: 'polygon'
 #'
 #' @export
 #'
 #' @seealso \code{\link{group_pts}} \code{\link{group_lines}} \code{\link{group_polys}}
-#' family
-
-
-#' importFrom
-
-#' examples
+#' @family Spatial grouping
+#' @importFrom data.table dcast
+#'
+#'
+#' @examples
+#' # GBI
 get_gbi <-
   function(DT = NULL,
            group = 'group',
@@ -34,6 +35,17 @@ get_gbi <-
       stop('type required')
     }
 
+    if (any(!(
+      c(group, id) %in% colnames(DT)
+    ))) {
+      stop(paste0(
+        as.character(paste(setdiff(
+          c(group, id),
+          colnames(DT)
+        ), collapse = ', ')),
+        ' field(s) provided are not present in input DT'
+      ))
+    }
 
     if (type == 'point') {
 
