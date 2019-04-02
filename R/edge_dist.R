0f572d17e9983208b930b40fda7dd696852d3a63
Alec Robitaille <robit.alec@gmail.com>
2019-04-01 17:08:46 -0700

    add edge_dist example

--- R/edge_dist.R
+++ R/edge_dist.R
@@ -9,6 +9,23 @@
 #'
 #' @family Edge-list generation
 #'
+#' @examples
+#' # Load data.table
+#' library(data.table)
+#'
+#' # Read example data
+#' DT <- fread(system.file("extdata", "DT.csv", package = "spatsoc"))
+#'
+#' # Cast the character column to POSIXct
+#' DT[, datetime := as.POSIXct(datetime, tz = 'UTC')]
+#'
+#' # Temporal grouping
+#' group_times(DT, datetime = 'datetime', threshold = '20 minutes')
+#'
+#' # Edge list generation
+#' group_pts(DT, threshold = 50, id = 'ID',
+#'           coords = c('X', 'Y'), timegroup = 'timegroup')
+#'
 edge_dist <- function(DT = NULL,
                       threshold = NULL,
                       id = NULL,
