a3baa74ac2d5245273cd0d5dc60ab128965eb416
Alec L. Robitaille <robit.alec@gmail.com>
2020-03-04 14:49:09 -0330

    fst group_dyad.R

---
+++ R/group_dyad.R
@@ -0,0 +1,122 @@
+#' Group Dyads
+#'
+#' @param DT input data.table
+#' @param threshold distance for grouping points, in the units of the coordinates
+#' @param id Character string of ID column name
+#' @param coords Character vector of X coordinate and Y coordinate column names
+#' @param timegroup timegroup field in the DT upon which the grouping will be calculated
+#' @param splitBy (optional) character string or vector of grouping column name(s) upon which the grouping will be calculated
+#'
+#' @export
+#'
+#' @family Spatial grouping
+#' @seealso \code{\link{group_times}}
+#'
+#' @examples
+#' # Load data.table
+#' library(data.table)
+#'
+#' # Read example data
+#' DT <- fread(system.file("extdata", "DT.csv", package = "spatsoc"))
+#'
+#' # Cast the character column to POSIXct
+#' DT[, datetime := as.POSIXct(datetime, tz = 'UTC')]
+#'
+#' # Temporal grouping
+#' group_times(DT, datetime = 'datetime', threshold = '20 minutes')
+#'
+#'
+group_dyad <- function(DT = NULL,
+                       threshold = NULL,
+                       id = NULL,
+                       coords = NULL,
+                       timegroup,
+                       splitBy = NULL) {
+  # due to NSE notes in R CMD check
+  N <- withinGroup <- ..id <- ..coords <- group <- NULL
+
+  if (is.null(DT)) {
+    stop('input DT required')
+  }
+
+  if (is.null(threshold)) {
+    stop('threshold required')
+  }
+
+  if (!is.numeric(threshold)) {
+    stop('threshold must be numeric')
+  }
+
+  if (threshold <= 0) {
+    stop('threshold must be greater than 0')
+  }
+
+  if (is.null(id)) {
+    stop('ID field required')
+  }
+
+  if (length(coords) != 2) {
+    stop('coords requires a vector of column names for coordinates X and Y')
+  }
+
+  if (missing(timegroup)) {
+    stop('timegroup required')
+  }
+
+  if (any(!(
+    c(timegroup, id, coords, splitBy) %in% colnames(DT)
+  ))) {
+    stop(paste0(
+      as.character(paste(setdiff(
+        c(timegroup, id, coords, splitBy),
+        colnames(DT)
+      ), collapse = ', ')),
+      ' field(s) provided are not present in input DT'
+    ))
+  }
+
+  if (any(!(DT[, vapply(.SD, is.numeric, TRUE), .SDcols = coords]))) {
+    stop('coords must be numeric')
+  }
+
+  if (!is.null(timegroup)) {
+    if (any(unlist(lapply(DT[, .SD, .SDcols = timegroup], class)) %in%
+            c('POSIXct', 'POSIXlt', 'Date', 'IDate', 'ITime', 'character'))) {
+      warning(
+        strwrap(
+          prefix = " ",
+          initial = "",
+          x = 'timegroup provided is a date/time
+          or character type, did you use group_times?'
+        )
+      )
+    }
+  }
+
+  if ('group' %in% colnames(DT)) {
+    message('group column will be overwritten by this function')
+    set(DT, j = 'group', value = NULL)
+  }
+
+  if (is.null(timegroup) && is.null(splitBy)) {
+    splitBy <- NULL
+  } else {
+    splitBy <- c(splitBy, timegroup)
+    if (DT[, .N, by = c(id, splitBy, timegroup)][N > 1, sum(N)] != 0) {
+      warning(
+        strwrap(
+          prefix = " ",
+          initial = "",
+          x = 'found duplicate id in a
+          timegroup and/or splitBy -
+          does your group_times threshold match the fix rate?'
+        )
+      )
+    }
+  }
+
+
+
+  # TODO: group_dyad logic
+
+}
