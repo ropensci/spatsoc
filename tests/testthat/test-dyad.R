60de2c111e29b289a7595912580bb67ff71dcf39
Alec L. Robitaille <robit.alec@gmail.com>
2020-03-25 18:08:09 -0300

    add test args are required

--- tests/testthat/test-dyad.R
+++ tests/testthat/test-dyad.R
@@ -11,5 +11,19 @@ edges <- edge_nn(DT,
                  timegroup = 'timegroup')
 
 
+test_that('DT is required', {
+  expect_error(dyad_id(DT = NULL),
+  'input DT required')
+})
+
+test_that('id1 and id2 are required', {
+  expect_error(dyad_id(DT = edges, id1 = NULL),
+               'input DT required')
+
+  expect_error(dyad_id(DT = edges, id1 = 'ID1', id2 = NULL),
+               'input DT required')
+})
+
+
 # TODO: Test if any are NULL
 # TODO: Test if id1 or id2 arent in DT
