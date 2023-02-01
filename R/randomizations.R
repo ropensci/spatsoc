8d781a6b8dfa233a00bcd8b2d9532a197719f5e6
Alec L. Robitaille <robit.alec@gmail.com>
2023-02-01 15:46:31 -0400

    fix c in randomizations

--- R/randomizations.R
+++ R/randomizations.R
@@ -224,7 +224,7 @@ randomizations <- function(DT = NULL,
   }
 
   repDT <- DT[, .SD, .SDcols = selCols][rep(1:.N, iterations + 1)]
-  repDT[, rowID := .GRP, by = selCols]
+  repDT[, rowID := .GRP, by = c(selCols)]
 
   if (any(repDT[, .N , by = rowID]$N != iterations + 1)) {
     warning('found non-unique rows of id, datetime (and splitBy)')
@@ -264,7 +264,7 @@ randomizations <- function(DT = NULL,
     }
 
 
-    idDays[, randomID := .SD[sample(.N, size = .N)], by = splitBy, .SDcols = id]
+    idDays[, randomID := .SD[sample(.N, size = .N)], by = c(splitBy), .SDcols = id]
     idDays[(observed), randomID := .SD[[1]], .SDcols = id]
 
     return(merge(repDT, idDays, on = splitBy, all = TRUE))
@@ -276,7 +276,7 @@ randomizations <- function(DT = NULL,
       splitBy <- c(id, 'iteration', splitBy)
     }
 
-    idDays[, randomJul := sample(jul, size = .N), by = splitBy]
+    idDays[, randomJul := sample(jul, size = .N), by = c(splitBy)]
 
     merged <- merge(
       x = repDT,
