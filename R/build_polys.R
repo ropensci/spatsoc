a03ca67b07e885c999d56db779f0a0e7d4b3cecc
Alec L. Robitaille <robit.alec@gmail.com>
2023-08-17 13:02:37 -0300

    show crs as numeric

--- R/build_polys.R
+++ R/build_polys.R
@@ -84,7 +84,7 @@
 #' DT[, datetime := as.POSIXct(datetime, tz = 'UTC')]
 #'
 #' # EPSG code for example data
-#' utm <- 'EPSG:32736'
+#' utm <- 32736
 #'
 #' # Build polygons for each individual using kernelUD and getverticeshr
 #' build_polys(DT, projection = utm, hrType = 'kernel',
