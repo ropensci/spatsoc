10de20a27ec3dbe4064b0fbb79af29f4f51ade16
Alec L. Robitaille <robit.alec@gmail.com>
2023-08-21 15:47:27 -0300

    use set units

--- tests/testthat/test-group-polys.R
+++ tests/testthat/test-group-polys.R
@@ -474,9 +474,12 @@ test_that('proportion within 0-100, area > 0', {
     id = 'ID'
   ))
 
-  expect_gte(min(out_mcp$proportion), units::as_units(0, 'percent'))
-  expect_lte(max(out_mcp$proportion), units::as_units(100, 'percent'))
-  expect_gte(min(out_mcp$area), units::as_units(0, 'percent'))
+  expect_gte(min(out_mcp$proportion),
+             units::set_units(0, 'percent', mode = 'standard'))
+  expect_lte(max(out_mcp$proportion),
+             units::set_units(100, 'percent', mode = 'standard'))
+  expect_gte(min(out_mcp$area),
+             units::set_units(0, 'percent', mode = 'standard'))
 
 
   out_kernel <- suppressWarnings(group_polys(
@@ -489,8 +492,11 @@ test_that('proportion within 0-100, area > 0', {
     id = 'ID'
   ))
 
-  expect_gte(min(out_kernel$proportion), units::as_units(0, 'percent'))
-  expect_lte(max(out_kernel$proportion), units::as_units(100, 'percent'))
-  expect_gte(min(out_kernel$area), units::as_units(0, 'percent'))
+  expect_gte(min(out_kernel$proportion),
+             units::set_units(0, 'percent', mode = 'standard'))
+  expect_lte(max(out_kernel$proportion),
+             units::set_units(100, 'percent', mode = 'standard'))
+  expect_gte(min(out_kernel$area),
+             units::set_units(0, 'percent', mode = 'standard'))
 
 })
