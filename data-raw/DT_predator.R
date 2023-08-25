c7c65a31ae271eca372b1e8bb35431f8c78646c3
Alec L. Robitaille <robit.alec@gmail.com>
2023-08-25 11:19:57 -0300

    less preds, translate xy

--- data-raw/DT_predator.R
+++ data-raw/DT_predator.R
@@ -1,9 +1,9 @@
-## code to prepare `DT_predator` dataset goes here
+library(data.table)
 DT <- fread('inst/extdata/DT.csv')
 
-DT_predator <- DT[ID %in% LETTERS[seq.int(3)]]
+DT_predator <- DT[ID %in% LETTERS[seq.int(2)]][sample(.N, 100)]
 
-DT_predator[, c('X_trans', 'Y_trans') := .(X + 1e4, Y - 5e4)]
+DT_predator[, c('X', 'Y') := .(X + 1e4, Y - 5e4)]
 DT_predator[, type := 'predator']
 
 # usethis::use_data(DT_predator, overwrite = TRUE)
--- data-raw/DT_prey.R
+++ data-raw/DT_prey.R
@@ -1,9 +1,9 @@
-## code to prepare `DT_prey` dataset goes here
+library(data.table)
 DT <- fread('inst/extdata/DT.csv')
 
 DT_prey <- DT[ID %in% LETTERS[seq(4, 7)]]
 
-DT_prey[, c('X_trans', 'Y_trans') := .(X + 1e4, Y - 5e4)]
+DT_prey[, c('X', 'Y') := .(X + 1e4, Y - 5e4)]
 DT_prey[, type := 'prey']
 
 # usethis::use_data(DT_prey, overwrite = TRUE)
