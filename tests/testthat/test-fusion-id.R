891c23c565b582da277c046b69bdc035b860e03b
Alec L. Robitaille <robit.alec@gmail.com>
2024-09-18 22:06:35 -0300

    setup

--- tests/testthat/test-fusion-id.R
+++ tests/testthat/test-fusion-id.R
@@ -1,1 +1,19 @@
 context("test-fusion-id")
+
+library(spatsoc)
+
+DT <- fread('../testdata/DT.csv')
+
+group_times(DT, datetime = 'datetime', threshold = '20 minutes')
+
+edges <- edge_dist(
+    DT,
+    threshold = 100,
+    id = 'ID',
+    coords = c('X', 'Y'),
+    timegroup = 'timegroup',
+    returnDist = TRUE,
+    fillNA = TRUE
+  )
+
+dyad_id(edges, 'ID1', 'ID2')
