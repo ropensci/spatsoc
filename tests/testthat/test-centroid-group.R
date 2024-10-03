5b94b1924b1669ebda74c15e6ad9e3c2d478994f
Alec L. Robitaille <robit.alec@gmail.com>
2024-10-03 14:27:53 -0300

    fst test centroid group

---
+++ tests/testthat/test-centroid-group.R
@@ -0,0 +1,11 @@
+# Test centroid_group
+context('test centroid_group')
+
+library(spatsoc)
+
+DT <- fread('../testdata/DT.csv')
+
+DT[, datetime := as.POSIXct(datetime, tz = 'UTC')]
+group_times(DT, datetime = 'datetime', threshold = '20 minutes')
+group_pts(DT, threshold = 50, id = 'ID', coords = c('X', 'Y'), timegroup = 'timegroup')
+
