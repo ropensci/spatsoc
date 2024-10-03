4203ab333d1ccda550ebecc7e17f28c7442d00de
Alec L. Robitaille <robit.alec@gmail.com>
2024-10-03 14:40:43 -0300

    test args required

--- tests/testthat/test-centroid-group.R
+++ tests/testthat/test-centroid-group.R
@@ -18,6 +18,16 @@ group_times(DT, datetime = datetime, threshold = timethreshold)
 group_pts(DT, threshold = threshold, id = id, coords = coords, timegroup = timegroup)
 
 clean_DT <- copy(DT)
+
 test_that('DT is required', {
   expect_error(centroid_group(DT = NULL))
+})
+
+test_that('arguments required, otherwise error detected', {
+  expect_error(centroid_group(DT, coords = 'X'),
+               'coords requires a vector')
+  expect_error(centroid_group(DT, coords = coords, group = NULL),
+               'group column name required')
+  expect_error(centroid_group(DT, coords = coords, na.rm = NULL),
+               'na.rm is required')
 })
