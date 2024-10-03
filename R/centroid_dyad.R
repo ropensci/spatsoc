4024b09b94dd4cbd774e571c99fac335c3a0153b
Alec L. Robitaille <robit.alec@gmail.com>
2024-10-03 16:48:04 -0300

    checks

--- R/centroid_dyad.R
+++ R/centroid_dyad.R
@@ -90,3 +90,55 @@ centroid_dyad <- function(
     coords = NULL,
     timegroup = 'timegroup',
     na.rm = FALSE) {
+
+  if (is.null(DT)) {
+    stop('input DT required')
+  }
+
+  if (is.null(edges)) {
+    stop('input edges required')
+  }
+
+  if (length(coords) != 2) {
+    stop('coords requires a vector of column names for coordinates X and Y')
+  }
+
+  if (is.null(timegroup)) {
+    stop('timegroup column name required')
+  }
+
+  if (any(!(
+    c('dyadID', timegroup) %in% colnames(edges)
+  ))) {
+    stop(paste0(
+      as.character(paste(setdiff(
+        c('dyadID', timegroup),
+        colnames(edges)
+      ), collapse = ', ')),
+      ' field(s) provided are not present in input DT'
+    ))
+  }
+
+  if (any(!(
+    c(id, coords, timegroup) %in% colnames(DT)
+  ))) {
+    stop(paste0(
+      as.character(paste(setdiff(
+        c(id, coords, timegroup),
+        colnames(DT)
+      ), collapse = ', ')),
+      ' field(s) provided are not present in input DT'
+    ))
+  }
+
+  if (any(!(DT[, vapply(.SD, is.numeric, TRUE), .SDcols = coords]))) {
+    stop('coords must be numeric')
+  }
+
+  if (is.null(na.rm)) {
+    stop('na.rm is required')
+  }
+
+  if (!is.logical(na.rm)) {
+    stop('na.rm should be a boolean (TRUE/FALSE), see ?mean')
+  }
