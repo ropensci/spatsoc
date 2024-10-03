78897eec47ca31ae3ec1a3b5757c5b3eb307d845
Alec L. Robitaille <robit.alec@gmail.com>
2024-10-03 14:17:27 -0300

    details

--- R/centroid_group.R
+++ R/centroid_group.R
@@ -1,13 +1,21 @@
 #' Group centroid
 #'
-#' Mean of individual locations in each group
+#' \code{centroid_group} calculates the centroid (mean location) of
+#' all individuals in each spatiotemporal group identified by
+#' \code{group_pts}. The function accepts a \code{data.table} with
+#' relocation data appended with a
+#'   \code{group} column from \code{group_pts}. Relocation data should be
+#' in two columns representing the X and Y coordinates.
 #'
-#' @param DT expecting group generated with eg. group_pts
-#' @param coords character vector of column names for x, y
-#' @param group group column default 'group'
-#' @param na.rm if NAs should be removed in calculating mean location
-centroid_group <- function(DT, coords, group = 'group', na.rm = FALSE) {
-  stopifnot(length(coords) == 2)
+#' The \code{DT} must be a \code{data.table}. If your data is a
+#' \code{data.frame}, you can convert it by reference using
+#' \code{\link[data.table:setDT]{data.table::setDT}}.
+#'
+#' The \code{coords} and \code{group}
+#' arguments expect the names of a column in \code{DT} which correspond to the
+#' X and Y coordinates and group columns. The \code{na.rm} argument is passed
+#' to the \code{mean} function to control if NA values are removed before
+#' calculation.
 
   xcol <- first(coords)
   ycol <- last(coords)
