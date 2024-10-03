1f0630a31d9509d0c79863f388f5c8a8f19a096a
Alec L. Robitaille <robit.alec@gmail.com>
2024-10-03 14:27:59 -0300

    test DT required

--- tests/testthat/test-centroid-group.R
+++ tests/testthat/test-centroid-group.R
@@ -9,3 +9,6 @@ DT[, datetime := as.POSIXct(datetime, tz = 'UTC')]
 group_times(DT, datetime = 'datetime', threshold = '20 minutes')
 group_pts(DT, threshold = 50, id = 'ID', coords = c('X', 'Y'), timegroup = 'timegroup')
 
+test_that('DT is required', {
+  expect_error(centroid_group(DT = NULL))
+})
