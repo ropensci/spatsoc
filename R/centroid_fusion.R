078adec5483d2a7af7e6514609f9a75a719a5859
Alec L. Robitaille <robit.alec@gmail.com>
2024-10-04 11:00:58 -0300

    column expectations

--- R/centroid_fusion.R
+++ R/centroid_fusion.R
@@ -13,3 +13,15 @@
 #' \code{data.frame}, you can convert it by reference using
 #' \code{\link[data.table:setDT]{data.table::setDT}} or by reassigning using
 #' \code{\link[data.table:data.table]{data.table::data.table}}.
+#'
+#' The \code{edges} and \code{DT} are internally merged in this function using
+#' the columns \code{timegroup} (from \code{group_times}) and \code{ID1} and
+#' \code{ID2} (in \code{edges}, from \code{dyad_id}) and \code{id} (in
+#' \code{DT}). This function expects a \code{fusionID} present, generated with
+#' the \code{fusion_id} function. The \code{timegroup} argument expects the
+#' names of a column in \code{edges} which correspond to the timegroup column.
+#' The \code{id}, \code{coords} and \code{timegroup} arguments expect the names
+#' of a column in \code{DT} which correspond to the id, X and Y coordinates and
+#' timegroup columns. The \code{na.rm} argument is passed to the \code{rowMeans}
+#' function to control if NA values are removed before calculation.
+#'
