fe7d53da0a2e4be1d646283e48e6481f1cc15e3d
Alec L. Robitaille <robit.alec@gmail.com>
2024-10-04 12:01:36 -0300

    fix links

--- R/fusion_id.R
+++ R/fusion_id.R
@@ -48,9 +48,9 @@
 #' @seealso \code{\link{edge_dist}}
 #' @references
 #' See examples of identifying fission-fusion events with spatiotemporal data:
-#'  * <doi:10.1111/ele.12457>
-#'  * <doi:10.1016/j.anbehav.2018.03.014>
-#'  * <doi:10.1890/08-0345.1>
+#'  * <https://doi.org/10.1111/ele.12457>
+#'  * <https://doi.org/10.1016/j.anbehav.2018.03.014>
+#'  * <https://doi.org/10.1890/08-0345.1>
 #' @examples
 #'
 #' # Load data.table
