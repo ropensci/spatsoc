748614352a4edd4ee98591d81a954df955e0f167
Alec L. Robitaille <robit.alec@gmail.com>
2024-10-04 12:50:59 -0300

    description, details

--- R/direction_step.R
+++ R/direction_step.R
@@ -1,1 +1,29 @@
 #' Calculate direction at each step
+#'
+#' \code{direction_step} calculates the direction of movement steps. The
+#' function accepts a \code{data.table} with relocation data and individual
+#' identifiers. Relocation data should be in two columns representing the X and
+#' Y coordinates. Note the order of rows is not modified by this function and
+#' therefore users must be cautious to set it explictly. See example for one
+#' approach to setting order of rows using a datetime field.
+#'
+#' The \code{DT} must be a \code{data.table}. If your data is a
+#' \code{data.frame}, you can convert it by reference using
+#' \code{\link[data.table:setDT]{data.table::setDT}} or by reassigning using
+#' \code{\link[data.table:data.table]{data.table::data.table}}.
+#'
+#' The \code{id}, \code{coords}, and optional \code{splitBy} arguments expect
+#' the names of a column in \code{DT} which correspond to the individual
+#' identifier, X and Y coordinates, and additional grouping columns.
+#'
+#' The \code{projection} argument expects a character string or numeric defining
+#' the coordinate reference system to be passed to [sf::st_crs]. For example,
+#' for UTM zone 36S (EPSG 32736), the projection argument is
+#' \code{projection = "EPSG:32736"} or \code{projection = 32736}. See
+#' <https://spatialreference.org> for #' a list of EPSG codes.
+#'
+#' The \code{splitBy} argument offers further control over grouping. If within
+#' your \code{DT}, you have distinct sampling periods for each individual, you
+#' can provide the column name(s) which identify them to \code{splitBy}. The
+#' direction calculation by \code{direction_step} will only consider rows within
+#' each \code{id} and \code{splitBy} subgroup.
