8f178aaa733e3f58bfba985bf1f3c22db6d1b742
Alec L. Robitaille <robit.alec@gmail.com>
2024-10-04 13:22:59 -0300

    setup

--- tests/testthat/test-direction-step.R
+++ tests/testthat/test-direction-step.R
@@ -1,2 +1,15 @@
 # Test direction_step
 context('test direction_step')
+
+library(spatsoc)
+
+DT <- fread('../testdata/DT.csv')
+
+DT[, datetime := as.POSIXct(datetime, tz = 'UTC')]
+setorder(DT, datetime)
+
+id <- 'ID'
+coords <- c('X', 'Y')
+projection <- 32736
+
+clean_DT <- copy(DT)
