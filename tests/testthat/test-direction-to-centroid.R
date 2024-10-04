4153cd9ec4516c3262602f97b7440c5a7a9ba17e
Alec L. Robitaille <robit.alec@gmail.com>
2024-10-04 16:22:39 -0300

    required

--- tests/testthat/test-direction-to-centroid.R
+++ tests/testthat/test-direction-to-centroid.R
@@ -19,3 +19,24 @@ group_pts(DT, threshold = threshold, id = id,
 centroid_group(DT, coords = coords, group = group, na.rm = TRUE)
 
 clean_DT <- copy(DT)
+
+test_that('DT is required', {
+  expect_error(direction_to_centroid(DT = NULL))
+})
+
+test_that('arguments required, otherwise error detected', {
+  expect_error(direction_to_centroid(DT, coords = NULL),
+               'coords req')
+})
+
+test_that('column names must exist in DT', {
+  expect_error(direction_to_centroid(DT, coords = rep('potato', 2)),
+               'potato field')
+})
+
+test_that('coords are correctly provided or error detected', {
+  expect_error(direction_to_centroid(DT, coords = c('X', NULL)),
+               'coords requires a vector')
+  expect_error(direction_to_centroid(DT, coords = c('X', 'ID')),
+               'coords must be numeric')
+})
