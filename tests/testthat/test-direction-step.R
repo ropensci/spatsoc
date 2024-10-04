7f2116be4e114fc49db82c33aeac2d7ab99d18c5
Alec L. Robitaille <robit.alec@gmail.com>
2024-10-04 13:23:04 -0300

    args required

--- tests/testthat/test-direction-step.R
+++ tests/testthat/test-direction-step.R
@@ -13,3 +13,20 @@ coords <- c('X', 'Y')
 projection <- 32736
 
 clean_DT <- copy(DT)
+
+test_that('DT is required', {
+  expect_error(direction_step(DT = NULL), 'input DT required')
+})
+
+test_that('args required else error', {
+  expect_error(direction_step(DT,  id = NULL), 'id column')
+
+  expect_error(direction_step(DT, id = id, coords = NULL), 'coords requir')
+
+  expect_error(direction_step(DT,  id = id, coords = coords, projection = NULL),
+               'projection required')
+
+  expect_error(direction_step(DT, id = id, coords = 'X'),
+               'coords requires a vector')
+})
+
