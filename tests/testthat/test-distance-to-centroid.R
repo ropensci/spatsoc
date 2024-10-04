805a2dcf2a664d7b57da39749c9ca1f482590d8a
Alec L. Robitaille <robit.alec@gmail.com>
2024-10-04 16:19:34 -0300

    setup

--- tests/testthat/test-distance-to-centroid.R
+++ tests/testthat/test-distance-to-centroid.R
@@ -1,2 +1,21 @@
 # Test distance_to_centroid
 context('test distance_to_centroid')
+
+library(spatsoc)
+
+DT <- fread('../testdata/DT.csv')
+id <- 'ID'
+datetime <- 'datetime'
+timethreshold <- '20 minutes'
+threshold <- 50
+coords <- c('X', 'Y')
+timegroup <- 'timegroup'
+group <- 'group'
+
+DT[, datetime := as.POSIXct(datetime, tz = 'UTC')]
+group_times(DT, datetime = datetime, timethreshold)
+group_pts(DT, threshold = threshold, id = id,
+          coords = coords, timegroup = timegroup)
+centroid_group(DT, coords = coords, group = group, na.rm = TRUE)
+
+clean_DT <- copy(DT)
