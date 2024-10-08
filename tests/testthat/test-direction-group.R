bd02e1fea134aa5267ec3119ede5a9b2f3437616
Alec L. Robitaille <robit.alec@gmail.com>
2024-10-08 10:30:38 -0300

    test dir numeric

--- tests/testthat/test-direction-group.R
+++ tests/testthat/test-direction-group.R
@@ -43,6 +43,11 @@ test_that('radians expected else error', {
                'direction_step')
 })
 
+test_that('direction expected numeric', {
+  expect_error(direction_group(DT, direction = 'ID'),
+               'direction must be numeric')
+})
+
 
 test_that('group_direction column succesfully detected', {
   copyDT <- copy(clean_DT)[, group_direction := 1]
