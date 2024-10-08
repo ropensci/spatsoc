114160c979d75f4fa996a2e3c56e7beaae1671c7
Alec L. Robitaille <robit.alec@gmail.com>
2024-10-08 11:11:49 -0300

    setup

--- tests/testthat/test-direction-polarization.R
+++ tests/testthat/test-direction-polarization.R
@@ -1,2 +1,21 @@
 # Test direction_polarization
 context('test direction_polarization')
+
+library(spatsoc)
+
+DT <- fread('../testdata/DT.csv')
+id <- 'ID'
+datetime <- 'datetime'
+timethreshold <- '20 minutes'
+threshold <- 50
+coords <- c('X', 'Y')
+timegroup <- 'timegroup'
+group <- 'group'
+projection <- 32736
+
+DT[, datetime := as.POSIXct(datetime, tz = 'UTC')]
+group_times(DT, datetime = datetime, threshold = timethreshold)
+group_pts(DT, threshold = threshold, id = id, coords = coords, timegroup = timegroup)
+direction_step(DT, id = id, coords = coords, projection = projection)
+
+clean_DT <- copy(DT)
