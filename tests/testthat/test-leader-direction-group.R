495d81e92e72563774c7fbe50c621a61f0e9e964
Alec L. Robitaille <robit.alec@gmail.com>
2024-10-29 19:30:29 -0300

    setup

--- tests/testthat/test-leader-direction-group.R
+++ tests/testthat/test-leader-direction-group.R
@@ -1,2 +1,25 @@
 # Test leader_direction_group
 context('test leader_direction_group')
+
+library(spatsoc)
+library(units)
+
+DT <- fread('../testdata/DT.csv')
+id <- 'ID'
+datetime <- 'datetime'
+timethreshold <- '20 minutes'
+threshold <- 50
+coords <- c('X', 'Y')
+timegroup <- 'timegroup'
+group <- 'group'
+projection <- 32736
+
+DT[, datetime := as.POSIXct(datetime, tz = 'UTC')]
+group_times(DT, datetime = datetime, timethreshold)
+group_pts(DT, threshold = threshold, id = id,
+          coords = coords, timegroup = timegroup)
+centroid_group(DT, coords = coords, group = group, na.rm = TRUE)
+direction_step(DT = DT, id = id, coords = coords, projection = projection)
+direction_group(DT, coords = coords)
+
+
