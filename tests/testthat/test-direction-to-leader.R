47473a3581943cf4d25eadddbbbcf5b608279bdf
Alec L. Robitaille <robit.alec@gmail.com>
2024-10-31 20:33:44 -0300

    test required

--- tests/testthat/test-direction-to-leader.R
+++ tests/testthat/test-direction-to-leader.R
@@ -24,3 +24,17 @@ leader_direction_group(DT, coords = coords, group = group, return_rank = TRUE)
 
 # Removing group with missing leader
 DT <- copy(DT)[group != 868]
+
+clean_DT <- copy(DT)
+
+test_that('DT is required', {
+  expect_error(distance_to_leader(DT = NULL))
+})
+
+test_that('arguments required, otherwise error detected', {
+  expect_error(distance_to_leader(DT, coords = NULL, group = group),
+               'coords req')
+  expect_error(distance_to_leader(DT, coords = coords, group = NULL),
+               'group column name required')
+})
+
