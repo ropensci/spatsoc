95949ba11e30789ef1203fccbe674719da8ddb1a
Alec L. Robitaille <robit.alec@gmail.com>
2024-10-31 20:22:17 -0300

    details

--- R/direction_to_leader.R
+++ R/direction_to_leader.R
@@ -8,3 +8,14 @@
 #' in planar coordinates provided in two columns representing the X and Y
 #' coordinates.
 #'
+#' The \code{DT} must be a \code{data.table}. If your data is a
+#' \code{data.frame}, you can convert it by reference using
+#' \code{\link[data.table:setDT]{data.table::setDT}} or by reassigning using
+#' \code{\link[data.table:data.table]{data.table::data.table}}.
+#'
+#' This function expects a \code{rank_position_group_direction} column
+#' generated with \code{leader_direction_group(return_rank = TRUE)},
+#' a \code{group} column generated with the
+#' \code{group_pts} function. The \code{coords} and \code{group} arguments
+#' expect the names of columns in \code{DT} which correspond to the X and Y
+#' coordinates and group columns.
