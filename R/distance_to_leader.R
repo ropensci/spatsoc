386276e298d8307664c0108c31e18f4968d19053
Alec L. Robitaille <robit.alec@gmail.com>
2024-10-31 19:33:19 -0300

    description

--- R/distance_to_leader.R
+++ R/distance_to_leader.R
@@ -1,7 +1,12 @@
 #' Distance to group leader
 #'
-#' Leader as identified using group_leader by first position along
-#' group axis of movement (return_rank = TRUE)
+#' \code{distance_to_leader} calculates the distance to the leader of each
+#' spatiotemporal group. The function accepts a \code{data.table} with
+#' relocation data appended with a \code{rank_position_group_direction} column
+#' indicating the ranked position along the group direction generated with
+#' \code{leader_direction_group(return_rank = TRUE)}. Relocation data should be
+#' in planar coordinates provided in two columns representing the X and Y
+#' coordinates.
 #'
 #' @param DT
 #' @param coords character vector of column names for x, y
