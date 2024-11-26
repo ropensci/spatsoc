c23f7869de11f0950cb47a99d8d780f4a5c6d6a8
Alec L. Robitaille <robit.alec@gmail.com>
2024-11-26 16:09:07 -0400

    test diff rad

--- tests/testthat/test-diff-rad.R
+++ tests/testthat/test-diff-rad.R
@@ -8,3 +8,13 @@ pt_02 <- as_units(0.2, 'rad')
 pi_rad <- as_units(pi, 'rad')
 
 
+expect_equal(diff_rad(pt_01, pt_02, TRUE), pt_01)
+expect_equal(diff_rad(pt_01, pt_02 + 2 * pi_rad, TRUE), pt_01)
+expect_equal(diff_rad(pt_01, pt_02 - 2 * pi_rad, TRUE), pt_01)
+expect_equal(diff_rad(pt_01 + 2 * pi_rad, pt_02, TRUE), pt_01)
+expect_equal(diff_rad(pt_01 - 2 * pi_rad, pt_02, TRUE), pt_01)
+expect_equal(diff_rad(pt_02, pt_01, TRUE), -pt_01)
+expect_equal(diff_rad(pt_02 + 2 * pi_rad, pt_01, TRUE), -pt_01)
+expect_equal(diff_rad(pt_02 - 2 * pi_rad, pt_01, TRUE), -pt_01)
+expect_equal(diff_rad(pt_02, pt_01 + 2 * pi_rad, TRUE), -pt_01)
+expect_equal(diff_rad(pt_02, pt_01 - 2 * pi_rad, TRUE), -pt_01)
