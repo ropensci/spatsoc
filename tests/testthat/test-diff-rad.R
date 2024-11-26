3f30aa1f808a243ed0d79c22ffe797ca11b90ddc
Alec L. Robitaille <robit.alec@gmail.com>
2024-11-26 16:16:27 -0400

    also test not signed

--- tests/testthat/test-diff-rad.R
+++ tests/testthat/test-diff-rad.R
@@ -7,7 +7,7 @@ pt_01 <- as_units(0.1, 'rad')
 pt_02 <- as_units(0.2, 'rad')
 pi_rad <- as_units(pi, 'rad')
 
-
+# Adapted from: https://gist.github.com/bradphelan/7fe21ad8ebfcb43696b8
 expect_equal(diff_rad(pt_01, pt_02, TRUE), pt_01)
 expect_equal(diff_rad(pt_01, pt_02 + 2 * pi_rad, TRUE), pt_01)
 expect_equal(diff_rad(pt_01, pt_02 - 2 * pi_rad, TRUE), pt_01)
@@ -18,3 +18,15 @@ expect_equal(diff_rad(pt_02 + 2 * pi_rad, pt_01, TRUE), -pt_01)
 expect_equal(diff_rad(pt_02 - 2 * pi_rad, pt_01, TRUE), -pt_01)
 expect_equal(diff_rad(pt_02, pt_01 + 2 * pi_rad, TRUE), -pt_01)
 expect_equal(diff_rad(pt_02, pt_01 - 2 * pi_rad, TRUE), -pt_01)
+
+expect_equal(diff_rad(pt_01, pt_02, FALSE), pt_01)
+expect_equal(diff_rad(pt_01, pt_02 + 2 * pi_rad, FALSE), pt_01)
+expect_equal(diff_rad(pt_01, pt_02 - 2 * pi_rad, FALSE), pt_01)
+expect_equal(diff_rad(pt_01 + 2 * pi_rad, pt_02, FALSE), pt_01)
+expect_equal(diff_rad(pt_01 - 2 * pi_rad, pt_02, FALSE), pt_01)
+expect_equal(diff_rad(pt_02, pt_01, FALSE), pt_01)
+expect_equal(diff_rad(pt_02 + 2 * pi_rad, pt_01, FALSE), pt_01)
+expect_equal(diff_rad(pt_02 - 2 * pi_rad, pt_01, FALSE), pt_01)
+expect_equal(diff_rad(pt_02, pt_01 + 2 * pi_rad, FALSE), pt_01)
+expect_equal(diff_rad(pt_02, pt_01 - 2 * pi_rad, FALSE), pt_01)
+
