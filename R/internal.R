51fb9c17deb9256b714dfeada3c8ec67d4bd8974
Alec L. Robitaille <robit.alec@gmail.com>
2024-11-26 11:06:47 -0400

    add example

--- R/internal.R
+++ R/internal.R
@@ -10,6 +10,29 @@
 #' @references adapted from https://stackoverflow.com/a/7869457
 #'
 #' @examples
+#' # Load data.table
+#' library(data.table)
+#' \dontshow{data.table::setDTthreads(1)}
+#'
+#' # Read example data
+#' DT <- fread(system.file("extdata", "DT.csv", package = "spatsoc"))
+#'
+#' # Cast the character column to POSIXct
+#' DT[, datetime := as.POSIXct(datetime, tz = 'UTC')]
+#'
+#' # Set order using data.table::setorder
+#' setorder(DT, datetime)
+#'
+#' # Calculate direction
+#' direction_step(
+#'   DT = DT,
+#'   id = 'ID',
+#'   coords = c('X', 'Y'),
+#'   projection = 32736
+#' )
+#'
+#' # Differences
+#' DT[, diff_rad(direction[1], direction[2])]
 diff_rad <- function(x, y,  signed = FALSE) {
   if (!inherits(x, 'units') || units(x)$numerator != 'rad') {
     stop('units(x) is not radians')
