461c7597bbfb6a99498c9fbdd469204f0338e108
Alec L. Robitaille <robit.alec@gmail.com>
2024-12-10 10:57:48 -0400

    description

--- R/leader_edge_delay.R
+++ R/leader_edge_delay.R
@@ -1,1 +1,5 @@
 #' Leadership in directional correlation delay
+#'
+#' Given the directional correlation delay, \code{leader_edge_delay}
+#' calculates the mean directional correlation delay for individuals in a
+#' group to identify leadership patterns.
