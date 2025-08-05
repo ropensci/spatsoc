ef49ee761c7090cf396099398be70b171cdbdbd1
Alec L. Robitaille <robit.alec@gmail.com>
2025-08-05 11:27:54 -0300

    setup

--- tests/testthat/test-edge-alignment.R
+++ tests/testthat/test-edge-alignment.R
@@ -1,2 +1,20 @@
 # Test edge_alignment
 context('test-edge-alignment')
+
+library(spatsoc)
+
+id <- 'ID'
+coords <- c('X', 'Y')
+direction <- 'direction'
+timegroup <- 'timegroup'
+group <- 'group'
+projection <- 32736
+
+DT <- fread('../testdata/DT.csv')
+group_times(DT, 'datetime', '10 minutes')
+direction_step(
+  DT = DT,
+  id = id,
+  coords = coords,
+  projection = projection
+)
