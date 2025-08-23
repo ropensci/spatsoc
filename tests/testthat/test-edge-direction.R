b3f055c493b77a7d105e1bbc67c54d6e32204a03
Alec L. Robitaille <robit.alec@gmail.com>
2025-08-23 10:01:23 -0300

    setup

--- tests/testthat/test-edge-direction.R
+++ tests/testthat/test-edge-direction.R
@@ -1,2 +1,24 @@
 # Test edge_direction
 context('test edge_direction')
+
+library(spatsoc)
+
+DT <- fread('../testdata/DT.csv')
+id <- 'ID'
+datetime <- 'datetime'
+timethreshold <- '20 minutes'
+threshold <- 50
+coords <- c('X', 'Y')
+timegroup <- 'timegroup'
+group <- 'group'
+projection <- 32736
+
+
+DT[, datetime := as.POSIXct(datetime, tz = 'UTC')]
+group_times(DT, datetime = datetime, threshold = timethreshold)
+edges <- edge_dist(DT, threshold = threshold, id = id, coords = coords,
+                   timegroup = timegroup, returnDist = TRUE, fillNA = FALSE)
+dyad_id(edges, id1 = 'ID1', id2 = 'ID2')
+
+clean_DT <- copy(DT)
+clean_edges <- copy(edges)
