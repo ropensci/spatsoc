e1044de9952fccb83ace9d5fd6620b00f5b28bae
Alec L. Robitaille <robit.alec@gmail.com>
2025-08-29 13:04:08 -0300

    fst edge zones logic

--- R/edge_zones.R
+++ R/edge_zones.R
@@ -15,3 +15,16 @@ edge_zones <- function(
   #  - zones
   #    - zone repulsion < orientation < attraction
   #  - blind_volume
+
+  edges[, zones := cut(distance, breaks = c(0, zone_thresholds),
+                       labels = c(zone_labels))]
+
+  if (!is.null(blind_volume)) {
+    edges[, direction_dyad_relative :=
+            diff_rad(direction, direction_dyad, signed = TRUE)]
+
+    edges[abs(direction_dyad_relative) > blind_volume & !is.na(zones),
+          zones := 'blind']
+  }
+
+}
