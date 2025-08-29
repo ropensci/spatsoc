ae8286f0bef863cc8b28e7e38c1397b15bf0fd12
Alec L. Robitaille <robit.alec@gmail.com>
2025-08-29 13:03:53 -0300

    args

--- R/edge_zones.R
+++ R/edge_zones.R
@@ -3,3 +3,8 @@
 #' @export
 #' TODO: @references (Couzin 2002, used in eg. Lukeman 2010, Klamser 2021, Klamser 2021)
 #' TODO: @inheritParams centroid_dyad
+edge_zones <- function(
+    edges = NULL,
+    zone_thresholds = NULL,
+    zone_labels = NULL,
+    blind_volume = NULL) {
