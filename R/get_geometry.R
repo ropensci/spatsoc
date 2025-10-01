697a404f47e587e8e64e75614c6eb31f59d9bb30
Alec L. Robitaille <robit.alec@gmail.com>
2025-10-01 16:48:35 -0300

    fst man

--- R/get_geometry.R
+++ R/get_geometry.R
@@ -1,1 +1,44 @@
 #' Get geometry
+#'
+#' `get_geometry` sets up an input `DT` with a 'geometry' column for {spatsoc}'s
+#' {sf} interface. The function expects a `data.table` with relocation data and
+#' a coordinate reference system.
+#'
+#' The `DT` must be a `data.table`. If your data is a `data.frame`, you can
+#' convert it by reference using [data.table::setDT()] or by reassigning using
+#' [data.table::data.table()].
+#'
+#' The `coords` argument expects the names of columns in `DT` which correspond
+#' to the X and Y coordinates.
+#'
+#' @return `get_geometry` returns the input `DT` appended with a
+#'   `geometry` column which represents the input coordinates
+#'   as a `sfc` (simple feature geometry list column). If the `output_crs`
+#'   was provided, the `geometry` will be transformed to the `output_crs`.
+#'
+#'   A message is returned when a column named `geometry` already exists in
+#'   the input `DT`, because it will be overwritten.
+#'
+#'   See details for appending outputs using modify-by-reference in the
+#'   [FAQ](https://docs.ropensci.org/spatsoc/articles/faq.html).
+#'
+#' @inheritParams group_pts
+#' @inheritParams group_polys
+#' @param output_crs default 4326, the output crs to transform the input
+#'   coordinates into. If output_crs is NULL or FALSE or matching the crs argument,
+#'   the coordinates will not be transformed
+#' @param geometry_colname default "geometry", optionally set output colname for
+#'   geometry column
+#'
+#' @export
+#'
+#' @examples
+#' # Load data.table
+#' library(data.table)
+#' \dontshow{data.table::setDTthreads(1)}
+#'
+#' # Read example data
+#' DT <- fread(system.file('extdata', 'DT.csv', package = 'spatsoc'))
+#'
+#' # Get geometry
+#' get_geometry(DT, coords = c('X', 'Y'), crs = 32736)
