787afc2811f25c947768b8c35159d6f0e649a77b
Alec L. Robitaille <robit.alec@gmail.com>
2025-10-01 17:11:33 -0300

    option with expects transformed

--- R/calc_direction.R
+++ R/calc_direction.R
@@ -11,27 +11,14 @@ calc_direction <- function(
     }
   } else if (missing(geometry_a) & !missing(x_a) & !missing(y_a)) {
     if (!missing(x_b) & !missing(y_b)) {
-      if (sf::st_is_longlat(crs)) {
-        lwgeom::st_geod_azimuth(
-          x = sf::st_as_sf(cbind(x_a, y_a), crs = crs),
-          y = sf::st_as_sf(cbind(x_b, y_b), crs = crs)
-        )
-      } else {
-        lwgeom::st_geod_azimuth(
-          x = sf::st_transform(sf::st_as_sf(cbind(x_a, y_a), crs = crs), 4326),
-          y = sf::st_transform(sf::st_as_sf(cbind(x_b, y_b), crs = crs), 4326)
-        )
-      }
+      lwgeom::st_geod_azimuth(
+        x = sf::st_as_sf(cbind(x_a, y_a), crs = crs),
+        y = sf::st_as_sf(cbind(x_b, y_b), crs = crs)
+      )
     } else {
-      if (sf::st_is_longlat(crs)) {
-        lwgeom::st_geod_azimuth(
-          x = sf::st_as_sf(cbind(x_a, y_a), crs = crs)
-        )
-      } else {
-        lwgeom::st_geod_azimuth(
-          x = sf::st_transform(sf::st_as_sf(cbind(x_a, y_a), crs = crs), 4326)
-        )
-      }
+      lwgeom::st_geod_azimuth(
+        x = sf::st_as_sf(cbind(x_a, y_a), crs = crs)
+      )
     }
   }
 }
