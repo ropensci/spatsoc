9884fc1913d98a05217f2e61ef30d89cc00b9032
Alec L. Robitaille <robit.alec@gmail.com>
2025-10-01 17:03:18 -0300

    rm prints

--- R/calc_direction.R
+++ R/calc_direction.R
@@ -4,19 +4,14 @@ calc_direction <- function(
     x_b, y_b) {
   if (!missing(geometry_a) & missing(x_a) & missing(y_a)) {
     if (!missing(geometry_b)) {
-      # TODO: rm the prints
-      print('pairwise')
       lwgeom::st_geod_azimuth(geometry_a, geometry_b)
     } else {
-      print('seq')
       lwgeom::st_geod_azimuth(geometry_a)
     }
   } else if (missing(geometry_a) & !missing(x_a) & !missing(y_a)) {
     if (!missing(x_b) & !missing(y_b)) {
-      print('pairwise')
       atan2(x_b - x_b, y_b - y_a)
     } else {
-      print('seq')
       atan2(diff(x_a), diff(y_a))
     }
   }
