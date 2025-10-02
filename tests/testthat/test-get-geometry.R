29532c78b3db2ab897bd8a6585b0a0311c7be275
Alec L. Robitaille <robit.alec@gmail.com>
2025-10-02 11:09:30 -0300

    test DT

--- tests/testthat/test-get-geometry.R
+++ tests/testthat/test-get-geometry.R
@@ -7,3 +7,10 @@ DT <- fread('../testdata/DT.csv')
 
 coords <- c('X', 'Y')
 crs <- 32736
+
+test_that('DT is required', {
+  expect_error(get_geometry(
+    DT = NULL
+  ),
+  'input DT required')
+})
