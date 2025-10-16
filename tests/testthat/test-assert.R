ec697a823784686c1ab4a42c58a0be845e06082f
Alec L. Robitaille <robit.alec@gmail.com>
2025-10-16 15:53:27 -0300

    add tests

--- tests/testthat/test-assert.R
+++ tests/testthat/test-assert.R
@@ -5,14 +5,23 @@ expect_error(assert_not_null(NULL))
 expect_error(assert_is_data_table(data.frame()))
 expect_error(assert_is_data_table(data.table()))
 
+test <- data.table(X = LETTERS, Y = LETTERS, ANGLE = runif(length(LETTERS), 0, 180))
+units(test$ANGLE) <- 'degrees'
 
-test <- data.table(X = LETTERS, Y = LETTERS)
 expect_error(assert_are_colnames(test, 'Z'))
 expect_error(assert_are_colnames(test, c('X', 'Z')))
 
-expect_error(assert_inherits(test, 'X', 'numeric'))
-expect_error(assert_inherits(test, c('X', 'Y'), 'numeric'))
+expect_error(assert_col_inherits(test, 'X', 'numeric'))
+expect_error(assert_col_inherits(test, c('X', 'Y'), 'numeric'))
 
+expect_error(assert_col_typeof(test, 'X', 'units'))
+
+expect_error(assert_inherits(42, 'character'))
+expect_error(assert_inherits(TRUE, 'numeric'))
+
+expect_error(assert_col_radians(test, 'ANGLE', ', did you use direction_step?'))
 expect_error(assert_length(1:2, 3L))
 
 expect_error(assert_relation(1, '>', 3))
+
+expect_error((function(x) assert_not_missing(x))())
