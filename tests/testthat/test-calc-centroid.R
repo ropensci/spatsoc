a783957cfce19cba1d3fe97d5b96a6761770f9c3
Alec L. Robitaille <robit.alec@gmail.com>
2025-11-26 20:33:06 -0400

    fix rename for clarity to use_mean

--- tests/testthat/test-calc-centroid.R
+++ tests/testthat/test-calc-centroid.R
@@ -74,28 +74,28 @@ test_that('calc_centroid equals st_centroid for mean and length 1 inputs', {
   i_seq <- DT[, sample(.I, 100)]
 
   expect_equal(
-    setnames(DT[i_seq, calc_centroid(x = X, y = Y, crs = crs, use_s2 = FALSE)], new = new_nms),
+    setnames(DT[i_seq, calc_centroid(x = X, y = Y, crs = crs, use_mean = TRUE)], new = new_nms),
     data.frame(st_coordinates(
       st_centroid(st_combine(st_as_sf(DT[i_seq, .(X, Y)], coords = seq.int(2), crs = crs)))
     ))
   )
 
   expect_equal(
-    setnames(DT[i, calc_centroid(x = X, y = Y, crs = crs, use_s2 = FALSE)], new = new_nms),
+    setnames(DT[i, calc_centroid(x = X, y = Y, crs = crs, use_mean = TRUE)], new = new_nms),
     data.frame(st_coordinates(
       st_centroid(st_combine(st_as_sf(DT[i, .(X, Y)], coords = seq.int(2), crs = crs)))
     ))
   )
 
   expect_equal(
-    setnames(DT[i_seq, calc_centroid(x = X, y = Y, crs = NA_crs_, use_s2 = FALSE)], new = new_nms),
+    setnames(DT[i_seq, calc_centroid(x = X, y = Y, crs = NA_crs_, use_mean = TRUE)], new = new_nms),
     data.frame(st_coordinates(
       st_centroid(st_combine(st_as_sf(DT[i_seq, .(X, Y)], coords = seq.int(2), crs = NA_crs_)))
     ))
   )
 
   expect_equal(
-    setnames(DT[i, calc_centroid(x = X, y = Y, crs = crs, use_s2 = FALSE)], , new = new_nms),
+    setnames(DT[i, calc_centroid(x = X, y = Y, crs = crs, use_mean = TRUE)], , new = new_nms),
     data.frame(st_coordinates(
       st_centroid(st_combine(st_as_sf(DT[i, .(X, Y)], coords = seq.int(2), crs = NA_crs_)))
     ))
