38352282129b78b7c71a0c65e3f3330ca487e964
Alec L. Robitaille <robit.alec@gmail.com>
2025-12-02 12:26:37 -0400

    fix NSE

--- R/distance_to_leader.R
+++ R/distance_to_leader.R
@@ -110,8 +110,8 @@ distance_to_leader <- function(
     geometry = 'geometry') {
 
   # Due to NSE notes
-  distance_leader <- zzz_N_by_group <- rank_position_group_direction <-
-    has_leader <- . <- NULL
+  geo <- lead <- x <- y <- x_leader <- y_leader <- . <-
+    rank_position_group_direction <- has_leader <- distance_leader <- NULL
 
   assert_not_null(DT)
   assert_is_data_table(DT)
