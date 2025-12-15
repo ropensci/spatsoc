2851cdf0be391d1973c5814735ff81cb8d7da7d9
Alec L. Robitaille <robit.alec@gmail.com>
2025-12-15 11:09:55 -0400

    test fix default output_crs = NULL

--- tests/testthat/test-calc-distance.R
+++ tests/testthat/test-calc-distance.R
@@ -4,25 +4,26 @@ context('test calc_distance')
 DT <- fread('../testdata/DT.csv')
 coords <- c('X', 'Y')
 crs <- 32736
-crs_lonlat <- 4326
+crs_longlat <- 4326
 
 # Reduce test data size
 DT <- DT[ID %in% c('A', 'B', 'C')]
 
 get_geometry(DT, coords = coords, crs = crs)
+get_geometry(DT, coords = coords, crs = crs, output_crs = 4326,
+             geometry_colname = 'geometry_longlat')
 
 DT[, dest_geometry := sf::st_centroid(sf::st_union(geometry))]
+DT[, dest_geometry_longlat := sf::st_centroid(sf::st_union(geometry_longlat))]
 
-lonlat_coords <- paste0('lonlat_', coords)
-DT[, (lonlat_coords) := as.data.table(sf::st_coordinates(geometry))]
+coords_longlat <- paste0(coords, '_longlat')
+DT[, (coords_longlat) := as.data.table(sf::st_coordinates(geometry_longlat))]
 
 dest_coords <- paste0('dest_', coords)
 DT[, (dest_coords) := as.data.table(sf::st_coordinates(dest_geometry))]
 
-# DT[, calc_distance(geometry_a = geometry, geometry_b = dest_geometry)]
-# DT[, calc_distance(x_a = lonlat_X, y_a = lonlat_Y,
-#                     x_b = dest_X, y_b = dest_Y,
-#                     crs = crs_lonlat)]
+dest_coords <- paste0('dest_', coords, '_longlat')
+DT[, (dest_coords) := as.data.table(sf::st_coordinates(dest_geometry_longlat))]
 
 test_that('arguments provided correctly else error', {
   expect_error(
@@ -45,7 +46,7 @@ test_that('arguments provided correctly else error', {
 test_that('units are returned', {
   expect_s3_class(DT[, calc_distance(geometry)], 'units')
   expect_s3_class(
-    DT[, calc_distance(x_a = lonlat_X, y_a = lonlat_Y, crs = crs_lonlat)],
+    DT[, calc_distance(x_a = X_longlat, y_a = Y_longlat, crs = crs_longlat)],
     'units'
   )
   expect_s3_class(
@@ -60,13 +61,13 @@ test_that('expected dims returned', {
   expect_length(DT[seq.int(N), calc_distance(geometry, dest_geometry)], N)
 
   expect_length(
-    DT[seq.int(N), calc_distance(x_a = lonlat_X, y_a = lonlat_Y, crs = crs_lonlat)],
+    DT[seq.int(N), calc_distance(x_a = X_longlat, y_a = Y_longlat, crs = crs_longlat)],
     N * N
   )
   expect_length(
-    DT[seq.int(N), calc_distance(x_a = lonlat_X, y_a = lonlat_Y,
-                                 x_b = dest_X, y_b = dest_Y,
-                                 crs = crs_lonlat)],
+    DT[seq.int(N), calc_distance(x_a = X_longlat, y_a = Y_longlat,
+                                 x_b = dest_X_longlat, y_b = dest_Y_longlat,
+                                 crs = crs_longlat)],
     N
   )
 })
@@ -78,9 +79,9 @@ test_that('expected range returned', {
                 units::set_units(0, 'm'))
   expect_gte(DT[, min(calc_distance(geometry, geometry_b = dest_geometry))],
              units::set_units(0, 'm'))
-  expect_gte(DT[, min(calc_distance(x_a = lonlat_X, y_a = lonlat_Y,
-                                    x_b = dest_X, y_b = dest_Y,
-                                    crs = crs_lonlat))],
+  expect_gte(DT[, min(calc_distance(x_a = X_longlat, y_a = Y_longlat,
+                                    x_b = dest_X_longlat, y_b = dest_Y_longlat,
+                                    crs = crs_longlat))],
              units::set_units(0, 'm'))
 })
 
@@ -97,13 +98,13 @@ test_that('NAs returned as expected', {
   expect_true(any(is.na(Y_NA$Y)))
   # expect_true(any(is.na(res)))
 
-  XY_NA <- copy(DT)[seq.int(100)][sample(.N, 10), (lonlat_coords) := NA]
-  res <- XY_NA[, calc_distance(x_a = lonlat_X, y_a = lonlat_Y, crs = 4326)]
+  XY_NA <- copy(DT)[seq.int(100)][sample(.N, 10), (coords_longlat) := NA]
+  res <- XY_NA[, calc_distance(x_a = X_longlat, y_a = Y_longlat, crs = 4326)]
   expect_length(res, nrow(XY_NA) * nrow(XY_NA))
-  expect_true(any(is.na(c(XY_NA$lonlat_X, XY_NA$lonlat_Y))))
+  expect_true(any(is.na(c(XY_NA$X_longlat, XY_NA$Y_longlat))))
   expect_true(any(is.na(res)))
 
-  get_geometry(XY_NA, lonlat_coords, crs_lonlat)
+  get_geometry(XY_NA, coords_longlat, crs_longlat)
   res <- XY_NA[, calc_distance(geometry)]
   expect_length(res, nrow(XY_NA) * nrow(XY_NA))
   expect_true(any(sf::st_is_empty(XY_NA$geometry)))
