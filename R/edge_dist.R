ca7b61d07de37b6418e3dd93c3f957091c9b3a83
Alec L. Robitaille <robit.alec@gmail.com>
2025-12-17 10:35:07 -0400

    tidy

--- R/edge_dist.R
+++ R/edge_dist.R
@@ -117,9 +117,7 @@ edge_dist <- function(
   assert_not_null(DT)
   assert_is_data_table(DT)
   assert_not_missing(threshold)
-
   assert_not_null(id)
-
   assert_not_missing(timegroup)
   assert_not_null(timegroup)
 
