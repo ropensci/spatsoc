36a17d2897cbd26eb4407bf7fe4676e784df7cfd
Alec L. Robitaille <robit.alec@gmail.com>
2026-04-24 16:24:04 -0230

    add full function list

--- R/spatsoc.R
+++ R/spatsoc.R
@@ -1,24 +1,69 @@
 #' spatsoc
 #'
 #' spatsoc is an R package for detecting spatial and temporal groups in GPS
-#' relocations. It can be used to convert GPS relocations to gambit-of-the-group
-#' format to build proximity-based social networks. In addition, the
-#' randomization function provides data-stream randomization methods suitable
-#' for GPS data.
+#' relocations and measuring intragroup social dynamics. It can be used to convert GPS relocations to gambit-of-the-group
+#' format to build proximity-based social networks, identify nearest neighbours distances and direction, and measure individual's position with respect to the group centroid or group leader. See all available functions below, in the manual or on the acommpanying documentation website at https://docs.ropensci.org/spatsoc/.
 #'
+#' Temporal grouping:
 #'
-#' The spatsoc package provides one temporal grouping function:
+#' - [`group_times()`]
 #'
-#' \itemize{ \item `group_times` } three spatial grouping functions:
-#' \itemize{ \item `group_pts` \item `group_lines` \item
-#' `group_polys` }
+#' Spatial grouping:
 #'
-#' two edge-list generating functions:
+#' - [`group_lines()`]
+#' - [`group_polys()`]
+#' - [`group_pts()`]
 #'
-#' \itemize{ \item `edge_dist` \item `edge_nn` }
+#' Edge-list generation:
 #'
-#' and two social network functions: \itemize{ \item
-#' `randomizations` \item `get_gbi` }
+#' - [`edge_dist()`]
+#' - [`edge_nn()`]
+#' - [`edge_delay()`]
+#' - [`edge_alignment()`]
+#' - [`edge_direction()`]
+#' - [`edge_zones()`]
+#'
+#' Social network tools:
+#'
+#' - [`randomizations()`]
+#' - [`get_gbi()`]
+#'
+#' Dyad functions
+#'
+#' - [`dyad_id()`]
+#' - [`fusion_id()`]
+#'
+#' Centroid functions
+#'
+#' - [`centroid_group()`]
+#' - [`centroid_dyad()`]
+#' - [`centroid_fusion()`]
+#'
+#' Direction functions
+#'
+#' - [`direction_step()`]
+#' - [`direction_to_centroid()`]
+#' - [`direction_to_leader()`]
+#' - [`direction_group()`]
+#' - [`direction_polarization()`]
+#'
+#' Distance functions
+#'
+#' - [`distance_to_centroid()`]
+#' - [`distance_to_leader()`]
+#'
+#' Leadership functions
+#'
+#' - [`leader_direction_group()`]
+#' - [`leader_edge_delay()`]
+#'
+#' Geometry interface functions
+#'
+#' - [`get_geometry()`]
+#'
+#' Build functions
+#' - [`build_lines()`]
+#' - [`build_polys()`]
 #'
 #' @docType package
 #' @name spatsoc
