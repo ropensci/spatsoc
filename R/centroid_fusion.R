6db6902722795ceb840d689b2f9456afb3972368
Alec L. Robitaille <robit.alec@gmail.com>
2026-04-25 21:05:36 -0230

    subset example data to reduce runtime

--- R/centroid_fusion.R
+++ R/centroid_fusion.R
@@ -60,6 +60,9 @@
 #' # Read example data
 #' DT <- fread(system.file("extdata", "DT.csv", package = "spatsoc"))
 #'
+#' # (Subset example data to reduce runtime on CRAN)
+#' DT <- DT[year(datetime) == 2016]
+#'
 #' # Cast the character column to POSIXct
 #' DT[, datetime := as.POSIXct(datetime, tz = 'UTC')]
 #'
